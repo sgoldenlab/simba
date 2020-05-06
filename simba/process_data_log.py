c17c9c4b751129084df1c48ce4cd341302dee68c
sgoldenlab <50497030+sgoldenlab@users.noreply.github.com>
2020-05-06 09:44:56 -0700

    Add files via upload

--- simba/process_data_log.py
+++ simba/process_data_log.py
@@ -3,31 +3,22 @@ import os
 from configparser import ConfigParser
 from datetime import datetime
 import numpy as np
+import glob
 
 
 def analyze_process_data_log(configini,chosenlist):
-
     dateTime = datetime.now().strftime('%Y%m%d%H%M%S')
     config = ConfigParser()
     configFile = str(configini)
     config.read(configFile)
-    csv_dir = config.get('General settings', 'csv_path')
-    csv_dir_in = os.path.join(csv_dir, 'machine_results')
+    projectPath = config.get('General settings', 'project_path')
+    csv_dir_in = os.path.join(projectPath, 'csv', 'machine_results')
     no_targets = config.getint('SML settings', 'No_targets')
-    filesFound = []
-    target_names = []
-    vidInfPath = config.get('General settings', 'project_path')
-    vidInfPath = os.path.join(vidInfPath, 'logs')
-    vidInfPath = os.path.join(vidInfPath, 'video_info.csv')
-    vidinfDf = pd.read_csv(vidInfPath)
-    loop = 0
-    loopy = 0
+    filesFound, target_names = [], []
+    vidinfDf = pd.read_csv(os.path.join(projectPath, 'logs', 'video_info.csv'))
+    loop, videoCounter = 0, 0
 
-    ########### FIND CSV FILES ###########
-    for i in os.listdir(csv_dir_in):
-        if i.endswith(".csv"):
-            file = os.path.join(csv_dir_in, i)
-            filesFound.append(file)
+    filesFound = glob.glob(csv_dir_in + '/*.csv')
 
     ########### GET TARGET COLUMN NAMES ###########
     for ff in range(no_targets):
@@ -38,35 +29,26 @@ def analyze_process_data_log(configini,chosenlist):
 
     ########### logfile path ###########
     log_fn = 'sklearn_' + str(dateTime) + '.csv'
-    log_path = config.get('General settings', 'project_path')
-    log_path = os.path.join(log_path, 'logs')
-    log_fn = os.path.join(log_path, log_fn)
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)
+    log_fn = os.path.join(projectPath, 'logs', log_fn)
 
     headers = ['Video']
-    headers_to_insert = [' first occurance (s)', ' # bout events', ' total events duration (s)', ' mean bout duration duration (s)', ' median bout duration duration (s)', ' mean interval (s)', ' median interval (s)']
+    headers_to_insert = [' # bout events', ' total events duration (s)',  ' mean bout duration duration (s)', ' median bout duration duration (s)', ' first occurance (s)', ' mean interval (s)', ' median interval (s)']
     for headerVar in headers_to_insert:
         for target in target_names:
             currHead = str(target) + headerVar
             headers.extend([currHead])
     log_df = pd.DataFrame(columns=headers)
 
-
-    for i in filesFound:
-        boutsDf = pd.DataFrame(columns=['Event', 'Start_frame', 'End_frame'])
-        currentFile = i
-        currVidName = os.path.basename(currentFile)
-        currVidName = currVidName.replace('.csv', '')
+    for currentFile in filesFound:
+        videoCounter += 1
+        currVidName = os.path.basename(currentFile).replace('.csv', '')
         fps = vidinfDf.loc[vidinfDf['Video'] == currVidName]
         try:
             fps = int(fps['fps'])
         except TypeError:
             print('Error: make sure all the videos that are going to be analyzed are represented in the project_folder/logs/video_info.csv file')
-        loopy+=1
-        print('Analyzing video ' + str(loopy) + '/' + str(len(filesFound)) + '...')
+        print('Analyzing video ' + str(videoCounter) + '/' + str(len(filesFound)) + '...')
         dataDf = pd.read_csv(currentFile)
-        dataDf['frames'] = np.arange(len(dataDf))
         boutsList, nameList, startTimeList, endTimeList = [], [], [], []
         for currTarget in target_names:
             groupDf = pd.DataFrame()
@@ -86,6 +68,7 @@ def analyze_process_data_log(configini,chosenlist):
         boutsDf = pd.DataFrame(list(zip(nameList, startTimeList, endTimeList, boutsList)), columns=['Event', 'Start Time', 'End Time', 'Duration'])
         boutsDf['Shifted start'] = boutsDf['Start Time'].shift(-1)
         boutsDf['Interval duration'] = boutsDf['Shifted start'] - boutsDf['End Time']
+
         firstOccurList, eventNOsList, TotEventDurList, MeanEventDurList, MedianEventDurList, TotEventDurList, meanIntervalList, medianIntervalList = [], [], [], [], [], [], [], []
         for targets in target_names:
             currDf = boutsDf.loc[boutsDf['Event'] == targets]
@@ -96,31 +79,37 @@ def analyze_process_data_log(configini,chosenlist):
             eventNOsList.append(len(currDf))
             TotEventDurList.append(round(currDf['Duration'].sum(), 3))
             try:
-                meanIntervalList.append(round(currDf['Interval duration'].mean(), 3))
+                if len(currDf) > 1:
+                    intervalDf = currDf[:-1].copy()
+                    meanIntervalList.append(round(intervalDf['Interval duration'].mean(), 3))
+                    medianIntervalList.append(round(intervalDf['Interval duration'].median(), 3))
+                else:
+                    meanIntervalList.append(0)
+                    medianIntervalList.append(0)
             except ZeroDivisionError:
                 meanIntervalList.append(0)
-            try:
-                medianIntervalList.append(round(currDf['Interval duration'].median(), 3))
-            except ZeroDivisionError:
                 medianIntervalList.append(0)
             try:
                 MeanEventDurList.append(round(currDf["Duration"].mean(), 3))
-            except ZeroDivisionError:
-                MeanEventDurList.append(0)
-            try:
                 MedianEventDurList.append(round(currDf['Duration'].median(), 3))
             except ZeroDivisionError:
+                MeanEventDurList.append(0)
                 MedianEventDurList.append(0)
-        currentVidList = [currVidName] + firstOccurList + eventNOsList + TotEventDurList + MeanEventDurList + MedianEventDurList + meanIntervalList + medianIntervalList
+        currentVidList = [currVidName] + eventNOsList + TotEventDurList + MeanEventDurList + MedianEventDurList + firstOccurList + meanIntervalList + medianIntervalList
+
         log_df.loc[loop] = currentVidList
         loop += 1
         print('File # processed for machine predictions: ' + str(loop) + '/' + str(len(filesFound)))
     log_df.fillna(0, inplace=True)
+    log_df.replace(0, np.NaN)
+
     # drop columns not chosen
-    for i in chosenlist:
-        log_df = log_df[log_df.columns.drop(list(log_df.filter(regex=str(i))))]
-
-
+    for target in target_names:
+        for col2drop in chosenlist:
+            currCol = target + ' ' + col2drop
+            print(currCol)
+            log_df = log_df.drop(currCol, 1)
+    print(log_df.columns)
     log_df.to_csv(log_fn, index=False)
     print('All files processed for machine predictions: data file saved @' + str(log_fn))
 
