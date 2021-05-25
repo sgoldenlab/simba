b8be64678f259973a03928c13c12c51e742c6b93
sgoldenlab <50497030+sgoldenlab@users.noreply.github.com>
2021-05-25 15:16:14 -0700

    Add files via upload

--- simba/outlier_scripts/skip_outlierCorrection.py
+++ simba/outlier_scripts/skip_outlierCorrection.py
@@ -1,26 +1,25 @@
 import pandas as pd
 import os
-import numpy as np
-import statistics
-import math
-from configparser import ConfigParser
+from configparser import ConfigParser, NoOptionError
 import glob
+import time
+from simba.rw_dfs import *
+from simba.drop_bp_cords import *
 
 
 def skip_outlier_c(configini):
     configFile = str(configini)
     config = ConfigParser()
     config.read(configFile)
+    try:
+        wfileType = config.get('General settings', 'workflow_file_type')
+    except NoOptionError:
+        wfileType = 'csv'
     projectPath = config.get('General settings', 'project_path')
-    currentBodyPartFile = os.path.join(projectPath, 'logs', 'measures', 'pose_configs', 'bp_names',
-                                       'project_bp_names.csv')
+    currentBodyPartFile = os.path.join(projectPath, 'logs', 'measures', 'pose_configs', 'bp_names', 'project_bp_names.csv')
     bodyPartsFile = pd.read_csv(os.path.join(currentBodyPartFile, currentBodyPartFile), header=None)
     bodyPartsList = list(bodyPartsFile[0])
-    bodyPartHeaders = []
-    xy_headers = []
-    p_cols = []
-    x_cols = []
-    y_cols = []
+    bodyPartHeaders, xy_headers, p_cols, x_cols, y_cols  = [], [], [], [], []
     for i in bodyPartsList:
         col1, col2, col3 = (str(i) + '_x', str(i) + '_y', str(i) + '_p')
         p_cols.append(col3)
@@ -34,18 +33,25 @@ def skip_outlier_c(configini):
     if not os.path.exists(csv_dir_out):
         os.makedirs(csv_dir_out)
 
-    filesFound = glob.glob(csv_dir_in + '/*.csv')
+    filesFound = glob.glob(csv_dir_in + '/*.' + wfileType)
+    print(filesFound)
 
-    for i in filesFound:
-        currentFile = i
-        baseNameFile = os.path.basename(currentFile).replace('.csv', '')
-        csv_df = pd.read_csv(currentFile, names=bodyPartHeaders, index_col=None)
-        csv_df = csv_df.drop(csv_df.index[[0, 1, 2]])
+    for currentFile in filesFound:
+        time.sleep(0.1)
+        baseNameFile = os.path.basename(currentFile).replace('.' + wfileType, '')
+        print('Processing file ' + baseNameFile)
+        csv_df = read_df(currentFile, wfileType)
+        if wfileType == 'csv':
+            csv_df = csv_df.drop(csv_df.index[[0, 1]])
         csv_df = csv_df.apply(pd.to_numeric)
-
-        csv_out = csv_df.rename_axis('scorer')
-        fileOut = str(baseNameFile) + str('.csv')
+        try:
+            csv_df = csv_df.set_index('scorer')
+        except KeyError:
+            pass
+        newHeaders = getBpHeaders(configini)
+        csv_df.columns = newHeaders
+        fileOut = str(baseNameFile) + '.' + wfileType
         pathOut = os.path.join(csv_dir_out, fileOut)
-        csv_out.to_csv(pathOut)
-
+        save_df(csv_df, wfileType, pathOut)
+        print('Saved ' + str(fileOut))
     print('CAUTION: Outlier corrections skipped. File headers corrected. Ready for the next step.')
