d42422f8bc9f58aea67d7267ff3e3a0bdc0350d5
Simon Nilsson <sronilsson@gmail.com>
2023-01-03 09:41:02 -0500

    Delete simba directory

--- simba/pose_import_initiators.py
+++
@@ -1,8 +0,0 @@
-
-
-def import_single_sleap_csv(config_path: str,
-                            file_path: str,
-                            actor_IDs: list,
-                            interpolation_settings: str,
-                            smoothing_settings: dict):
-
