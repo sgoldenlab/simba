dd1ae479c813a8e781c4d6299ad5b6efee820e8a
Simon Nilsson <sronilsson@gmail.com>
2023-05-19 17:18:20 -0400

    Add files via upload

--- simba/data_processors/interpolation_smoothing.py
+++ simba/data_processors/interpolation_smoothing.py
@@ -74,13 +74,16 @@ class Interpolate(ConfigReader):
             video_timer.start_timer()
             _, video_name, _ = get_fn_ext(filepath=file_path)
             df = read_df(file_path=file_path, file_type=self.file_type, check_multiindex=self.initial_import_multi_index)
+            df[df < 0] = 0
             if self.initial_import_multi_index:
-                df.columns = self.bp_col_names
+                df.columns = self.bp_headers
             for animal_name, animal_bps in self.animal_bp_dict.items():
                 animal_df = df[animal_bps['X_bps'] + animal_bps['Y_bps']].fillna(0).astype(int)
                 idx = list(animal_df[animal_df.eq(animal_df.iloc[:, 0], axis=0).all(axis='columns')].index)
+                print(f'Interpolating {len(idx)} body-parts for animal {animal_name} in video {video_name}...')
                 animal_df.loc[idx, :] = np.nan
                 animal_df = animal_df.interpolate(method=self.interpolation_method, axis=0).ffill().bfill()
+                animal_df[animal_df < 0] = 0
                 df.update(animal_df)
             if not self.initial_import_multi_index:
                 shutil.move(src=file_path, dst=os.path.join(self.save_dir, os.path.basename(file_path)))
@@ -98,13 +101,19 @@ class Interpolate(ConfigReader):
             _, video_name, _ = get_fn_ext(filepath=file_path)
             df = read_df(file_path=file_path, file_type=self.file_type, check_multiindex=True)
             if self.initial_import_multi_index:
-                df.columns = self.bp_col_names
+                df.columns = self.bp_headers
+            df[df < 0] = 0
             for animal in self.animal_bp_dict:
                 for x_bps_name, y_bps_name in zip(self.animal_bp_dict[animal]['X_bps'], self.animal_bp_dict[animal]['Y_bps']):
-                    idx = (df[(df[x_bps_name] == 0) & (df[y_bps_name] == 0)].index.tolist())
+                    df[x_bps_name] = df[x_bps_name].astype(int)
+                    df[y_bps_name] = df[y_bps_name].astype(int)
+                    idx = df.loc[(df[x_bps_name] <= 0.0) & (df[y_bps_name] <= 0.0)].index.tolist()
+                    print(f'Interpolating {len(idx)} {x_bps_name[:-2]} body-parts for animal {animal} in video {video_name}...')
                     df.loc[idx, [x_bps_name, y_bps_name]] = np.nan
                     df[x_bps_name] = df[x_bps_name].interpolate(method=self.interpolation_method, axis=0).ffill().bfill()
+                    df[x_bps_name][df[x_bps_name] < 0] = 0
                     df[y_bps_name] = df[y_bps_name].interpolate(method=self.interpolation_method, axis=0).ffill().bfill()
+                    df[y_bps_name][df[y_bps_name] < 0] = 0
             if not self.initial_import_multi_index:
                 shutil.move(src=file_path, dst=os.path.join(self.save_dir, os.path.basename(file_path)))
             else:
@@ -151,7 +160,7 @@ class Smooth(ConfigReader):
         if not initial_import_multi_index:
             self.save_dir = os.path.join(self.input_dir, f'Pre_{smoothing_method}_interpolation_{self.datetime}')
             if not os.path.exists(self.save_dir): os.makedirs(self.save_dir)
-        self.time_window, self.initial_import_multi_index = time_window, initial_import_multi_index
+        self.time_window, self.initial_import_multi_index = int(time_window), initial_import_multi_index
         if smoothing_method == Methods.SAVITZKY_GOLAY.value:
             self.savgol_smoother()
         elif smoothing_method == Methods.GAUSSIAN.value:
@@ -166,15 +175,24 @@ class Smooth(ConfigReader):
             df = read_df(file_path=file_path, file_type=self.file_type, check_multiindex=self.initial_import_multi_index)
             video_path = find_video_of_file(video_dir=self.video_dir, filename=video_name)
             if not video_path:
-                raise NoFilesFoundError(msg=f'No video for file {video_name} found in SimBA project. Import the video before doing smoothing. To perform smoothing, SimBA needs the video in order to read the video FPS.')
-            video_meta_data = get_video_meta_data(video_path=video_path)
-            frames_in_time_window = int(self.time_window / (1000 / video_meta_data['fps']))
+                try:
+                    video_meta_data = {}
+                    self.video_info_df = self.read_video_info_csv(file_path=self.video_info_path)
+                    _, _, fps = self.read_video_info(video_name=video_name)
+                    video_meta_data['fps'] = fps
+                except:
+                    raise NoFilesFoundError(msg=f'No video for file {video_name} found in SimBA project. Import the video before doing smoothing. To perform smoothing, SimBA needs the video fps from the video itself or the logs/video_info.csv file in order to read the video FPS.')
+            else:
+                video_meta_data = get_video_meta_data(video_path=video_path)
+            frames_in_time_window = int(self.time_window / (1000 / int(video_meta_data['fps'])))
             if (frames_in_time_window % 2) == 0:
                 frames_in_time_window = frames_in_time_window - 1
             if (frames_in_time_window % 2) <= 3:
                 frames_in_time_window = 5
+            df[df < 0] = 0
             for c in df.columns:
                 df[c] = savgol_filter(x=df[c].to_numpy(), window_length=frames_in_time_window, polyorder=3, mode='nearest')
+                df[c][df[c] < 0] = 0
             if not self.initial_import_multi_index:
                 shutil.move(src=file_path, dst=os.path.join(self.save_dir, os.path.basename(file_path)))
             else:
@@ -197,6 +215,7 @@ class Smooth(ConfigReader):
             frames_in_time_window = int(self.time_window / (1000 / video_meta_data['fps']))
             for c in df.columns:
                 df[c] = df[c].rolling(window=int(frames_in_time_window), win_type='gaussian', center=True).mean(std=5).fillna(df[c]).abs()
+                df[c][df[c] < 0] = 0
             if not self.initial_import_multi_index:
                 shutil.move(src=file_path, dst=os.path.join(self.save_dir, os.path.basename(file_path)))
             else:
@@ -208,11 +227,22 @@ class Smooth(ConfigReader):
             print(f'Video {video_name} smoothed (Gaussian: {str(self.time_window)}ms) (elapsed time {video_timer.elapsed_time_str})...')
 
 
+# Interpolate(input_path='/Users/simon/Desktop/envs/troubleshooting/two_black_animals_14bp/project_folder/csv/outlier_corrected_movement_location',
+#             config_path='/Users/simon/Desktop/envs/troubleshooting/two_black_animals_14bp/project_folder/project_config.ini',
+#             method='Body-parts: Nearest',
+#             initial_import_multi_index=False)
 
-# Interpolate(input_path='/Users/simon/Desktop/envs/troubleshooting/DLC_2_Black_animals/project_folder/csv/input_csv',
-#             config_path='/Users/simon/Desktop/envs/troubleshooting/DLC_2_Black_animals/project_folder/project_config.ini',
-#             method='Animal(s):Quadratic',
+# Interpolate(input_path='/Users/simon/Desktop/envs/troubleshooting/dorian_2/project_folder/csv/input_csv',
+#             config_path='/Users/simon/Desktop/envs/troubleshooting/dorian_2/project_folder/project_config.ini',
+#             method='Body-parts: Linear',
 #             initial_import_multi_index=True)
+#
+
+
+# Interpolate(input_path='/Users/simon/Desktop/envs/troubleshooting/sleap_5_animals/project_folder/csv/outlier_corrected_movement_location',
+#             config_path='/Users/simon/Desktop/envs/troubleshooting/sleap_5_animals/project_folder/project_config.ini',
+#             method='Body-parts: Nearest',
+#             initial_import_multi_index=False)
 
 # PostHocInterpolate(config_path='/Users/simon/Desktop/envs/troubleshooting/ddddfff/project_folder/project_config.ini',
 #                    input_dir='/Users/simon/Desktop/envs/troubleshooting/ddddfff/project_folder/csv/outlier_corrected_movement_location',
@@ -225,7 +255,11 @@ class Smooth(ConfigReader):
 #               smoothing_method=Methods.SAVITZKY_GOLAY.value,
 #               initial_import_multi_index=True)
 
-
+# Smooth(config_path='/Users/simon/Desktop/envs/troubleshooting/dorian_2/project_folder/project_config.ini',
+#               input_path='/Users/simon/Desktop/envs/troubleshooting/dorian_2/project_folder/csv/input_csv',
+#               time_window=400,
+#               smoothing_method=Methods.SAVITZKY_GOLAY.value,
+#               initial_import_multi_index=True)
 
 
 # class PostHocSmooth(ConfigReader):
