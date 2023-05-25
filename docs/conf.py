b7d1cce99447edf120bd98b32142c7aa0c137d6d
Simon Nilsson <sronilsson@gmail.com>
2023-05-25 08:58:27 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ intersphinx_mapping = {
 html_favicon = "../simba/assets/icons/SimBA_logo.png"
 html_logo = "../simba/assets/icons/SimBA_logo.png"
 latex_engine = 'xelatex'
+latex_elements = {'papersize': 'a4paper')
 
 
 # source_suffix = {
