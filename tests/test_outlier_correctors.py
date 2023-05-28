63845d408819a02229711814866f4d8578a2de6a
Simon Nilsson <sronilsson@gmail.com>
2023-05-28 10:23:20 -0400

    Update test_outlier_correctors.py

--- tests/test_outlier_correctors.py
+++ tests/test_outlier_correctors.py
@@ -5,21 +5,21 @@ from simba.outlier_tools.outlier_corrector_location import OutlierCorrecterLocat
 from simba.outlier_tools.outlier_corrector_movement import OutlierCorrecterMovement
 from simba.outlier_tools.skip_outlier_correction import OutlierCorrectionSkipper
 
-@pytest.mark.parametrize("config_path", ['/tests/data/test_projects/two_c57/project_folder/project_config.ini'])
+@pytest.mark.parametrize("config_path", ['tests/data/test_projects/two_c57/project_folder/project_config.ini'])
 def test_outlier_corrector_movement_use_case(config_path):
     movement_corrector = OutlierCorrecterMovement(config_path=config_path)
     movement_corrector.run()
     assert os.path.isfile(movement_corrector.logs_path)
     os.remove(movement_corrector.logs_path)
 
-@pytest.mark.parametrize("config_path", ['/tests/data/test_projects/two_c57/project_folder/project_config.ini'])
+@pytest.mark.parametrize("config_path", ['tests/data/test_projects/two_c57/project_folder/project_config.ini'])
 def test_outlier_corrector_location_use_case(config_path):
     location_corrector = OutlierCorrecterLocation(config_path=config_path)
     location_corrector.run()
     assert os.path.isfile(location_corrector.logs_path)
     os.remove(location_corrector.logs_path)
 
-@pytest.mark.parametrize("config_path", ['/tests/data/test_projects/two_c57/project_folder/project_config.ini'])
+@pytest.mark.parametrize("config_path", ['tests/data/test_projects/two_c57/project_folder/project_config.ini'])
 def test_outlier_skipper_use_case(config_path):
     skipper = OutlierCorrectionSkipper(config_path=config_path)
     skipper.run()
