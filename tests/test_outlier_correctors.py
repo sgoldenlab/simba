58605bf44e99c0cb67351d124a72119c022cd1af
Simon Nilsson <sronilsson@gmail.com>
2023-05-28 18:01:21 -0400

    Update test_outlier_correctors.py

--- tests/test_outlier_correctors.py
+++ tests/test_outlier_correctors.py
@@ -9,15 +9,11 @@ from simba.outlier_tools.skip_outlier_correction import OutlierCorrectionSkipper
 def test_outlier_corrector_movement_use_case(config_path):
     movement_corrector = OutlierCorrecterMovement(config_path=config_path)
     movement_corrector.run()
-    assert os.path.isfile(movement_corrector.logs_path)
-    os.remove(movement_corrector.logs_path)
 
 @pytest.mark.parametrize("config_path", ['tests/data/test_projects/two_c57/project_folder/project_config.ini'])
 def test_outlier_corrector_location_use_case(config_path):
     location_corrector = OutlierCorrecterLocation(config_path=config_path)
     location_corrector.run()
-    assert os.path.isfile(location_corrector.logs_path)
-    os.remove(location_corrector.logs_path)
 
 @pytest.mark.parametrize("config_path", ['tests/data/test_projects/two_c57/project_folder/project_config.ini'])
 def test_outlier_skipper_use_case(config_path):
