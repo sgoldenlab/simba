ac69925f9b75ad69fb9bcd98c227512ac996680d
Simon Nilsson <sronilsson@gmail.com>
2023-06-08 14:00:07 -0400

    Update test_video_processors.py

--- tests/test_video_processors.py
+++ tests/test_video_processors.py
@@ -1,9 +1,8 @@
 import pytest
 from simba.video_processors.batch_process_create_ffmpeg_commands import FFMPEGCommandCreator
 
-@pytest.mark.parametrize('json_path', ['/Users/simon/Desktop/envs/simba_dev/tests/data/test_projects/two_c57/video_processing/batch_process_log.json'])
+@pytest.mark.parametrize('json_path', ['tests/data/test_projects/two_c57/video_processing/batch_process_log.json'])
 def test_ffmpeg_command_creator(json_path):
-    print(json_path)
     ffmpeg_executor = FFMPEGCommandCreator(json_path=json_path)
     ffmpeg_executor.crop_videos()
     ffmpeg_executor.clip_videos()
