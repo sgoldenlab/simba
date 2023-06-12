22cb19ec1f1658b485cfd99a268a9c46d32f3266
Simon Nilsson <sronilsson@gmail.com>
2023-06-12 12:39:51 -0400

    Update test_roi_tools.py

--- tests/test_roi_tools.py
+++ tests/test_roi_tools.py
@@ -9,13 +9,13 @@ from simba.roi_tools.ROI_size_calculations import rectangle_size_calc, circle_si
 from simba.roi_tools.ROI_time_bin_calculator import ROITimebinCalculator
 
 
-@pytest.fixture(params=['/Users/simon/Desktop/envs/simba_dev/tests/data/test_projects/two_c57/project_folder/project_config.ini'])
+@pytest.fixture(params=['tests/data/test_projects/two_c57/project_folder/project_config.ini'])
 def config_path_args(request):
     return request
 
-@pytest.fixture(params=['/Users/simon/Desktop/envs/simba_dev/tests/data/test_projects/two_c57/project_folder/project_config.ini',
-                        '/Users/simon/Desktop/envs/simba_dev/tests/data/test_projects/mouse_open_field/project_folder/project_config.ini',
-                        '/Users/simon/Desktop/envs/simba_dev/tests/data/test_projects/zebrafish/project_folder/project_config.ini'])
+@pytest.fixture(params=['tests/data/test_projects/two_c57/project_folder/project_config.ini',
+                        'tests/data/test_projects/mouse_open_field/project_folder/project_config.ini',
+                        'tests/data/test_projects/zebrafish/project_folder/project_config.ini'])
 def all_config_path_args(request):
     return request
 
@@ -32,12 +32,12 @@ def test_roi_clf_calculator(config_path_args):
     clf_ROI_analyzer = ROIClfCalculator(config_ini=config_path_args.param)
     clf_ROI_analyzer.run(behavior_list=['Attack', 'Sniffing'], ROI_dict_lists={'Rectangle': ['Rectangle_1']}, body_part_list=['Nose_1'], measurements=['Total time by ROI (s)', 'Started bouts by ROI (count)', 'Ended bouts by ROI (count)'])
 
-@pytest.mark.parametrize("config_path, settings", [('/Users/simon/Desktop/envs/simba_dev/tests/data/test_projects/two_c57/project_folder/project_config.ini', {'body_parts': {'Simon': 'Ear_left_1', 'JJ': 'Ear_left_2'}, 'threshold': 0.4})])
+@pytest.mark.parametrize("config_path, settings", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', {'body_parts': {'Simon': 'Ear_left_1', 'JJ': 'Ear_left_2'}, 'threshold': 0.4})])
 def test_roi_directing_analyzer(config_path, settings):
     directing_analyzer = DirectingROIAnalyzer(config_path=config_path, settings=settings)
     directing_analyzer.run()
 
-@pytest.mark.parametrize("config_path, settings", [('/Users/simon/Desktop/envs/simba_dev/tests/data/test_projects/two_c57/project_folder/project_config.ini', {'body_parts': {'Simon': 'Ear_left_1', 'JJ': 'Ear_left_2'}, 'threshold': 0.4})])
+@pytest.mark.parametrize("config_path, settings", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', {'body_parts': {'Simon': 'Ear_left_1', 'JJ': 'Ear_left_2'}, 'threshold': 0.4})])
 def test_roi_feature_creator(config_path, settings):
     roi_featurizer = ROIFeatureCreator(config_path=config_path, settings=settings)
     roi_featurizer.run()
