9d702f8a076899119f6b44d16d1b875180d255d6
sronilsson <sronilsson@users.noreply.github.com>
2023-07-08 18:16:33 +0000

    cleaned

--- simba/roi_tools/ROI_size_calculations.py
+++ simba/roi_tools/ROI_size_calculations.py
@@ -1,8 +1,9 @@
-import numpy as np
 import math
 
-def rectangle_size_calc(rectangle_dict: dict,
-                        px_mm: float) -> dict:
+import numpy as np
+
+
+def rectangle_size_calc(rectangle_dict: dict, px_mm: float) -> dict:
     """
     Compute metric height, width and area of rectangle.
 
@@ -15,11 +16,13 @@ def rectangle_size_calc(rectangle_dict: dict,
 
     """
 
-
-    rectangle_dict['height_cm'] = round((rectangle_dict['height'] / px_mm) / 10, 2)
-    rectangle_dict['width_cm'] = round((rectangle_dict['width'] / px_mm) / 10, 2)
-    rectangle_dict['area_cm'] = round(rectangle_dict['width_cm'] * rectangle_dict['height_cm'], 2)
+    rectangle_dict["height_cm"] = round((rectangle_dict["height"] / px_mm) / 10, 2)
+    rectangle_dict["width_cm"] = round((rectangle_dict["width"] / px_mm) / 10, 2)
+    rectangle_dict["area_cm"] = round(
+        rectangle_dict["width_cm"] * rectangle_dict["height_cm"], 2
+    )
     return rectangle_dict
+
 
 def circle_size_calc(circle_dict, px_mm) -> dict:
     """
@@ -33,11 +36,11 @@ def circle_size_calc(circle_dict, px_mm) -> dict:
     >>> {'radius': 100, 'radius_cm': 2.0, 'area_cm': 12.57}
     """
 
-
-    radius_cm = round((circle_dict['radius'] / px_mm) / 10, 2)
-    circle_dict['radius_cm'] = radius_cm
-    circle_dict['area_cm'] = round(math.pi * (radius_cm ** 2), 2)
+    radius_cm = round((circle_dict["radius"] / px_mm) / 10, 2)
+    circle_dict["radius_cm"] = radius_cm
+    circle_dict["area_cm"] = round(math.pi * (radius_cm**2), 2)
     return circle_dict
+
 
 def polygon_size_calc(polygon_dict, px_mm) -> dict:
     """
@@ -51,14 +54,17 @@ def polygon_size_calc(polygon_dict, px_mm) -> dict:
     >>> {'vertices': [[  0,   2], [200,  98], [100, 876], [ 10, 702]], 'area_cm': 45.29}
     """
 
-
-
-    y_vals = polygon_dict['vertices'][:,0]
-    x_vals = polygon_dict['vertices'][:,1]
-    poly_area_px = 0.5 * np.abs(np.dot(x_vals, np.roll(y_vals, 1)) - np.dot(y_vals, np.roll(x_vals, 1)))
-    polygon_dict['area_cm'] = round((poly_area_px / px_mm) / 500, 2)
+    y_vals = polygon_dict["vertices"][:, 0]
+    x_vals = polygon_dict["vertices"][:, 1]
+    poly_area_px = 0.5 * np.abs(
+        np.dot(x_vals, np.roll(y_vals, 1)) - np.dot(y_vals, np.roll(x_vals, 1))
+    )
+    polygon_dict["area_cm"] = round((poly_area_px / px_mm) / 500, 2)
 
     return polygon_dict
 
 
-polygon_size_calc(polygon_dict={'vertices': np.array([[0, 2], [200, 98], [100, 876], [10, 702]])}, px_mm=5)
+polygon_size_calc(
+    polygon_dict={"vertices": np.array([[0, 2], [200, 98], [100, 876], [10, 702]])},
+    px_mm=5,
+)
