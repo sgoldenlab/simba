b1f97486bcacefd9e9794ae10a6c85f524710c8f
Simon Nilsson <sronilsson@gmail.com>
2023-08-06 14:29:34 -0400

    Add files via upload

--- simba/feature_extractors/perimeter_jit.py
+++ simba/feature_extractors/perimeter_jit.py
@@ -1,34 +1,35 @@
 __author__ = "Simon Nilsson"
 
 import numpy as np
-from numba import jit, njit, prange
+from numba import njit, jit, prange
 from numba.np.extensions import cross2d
 
-
-@njit("(float32[:,:], int64[:], int64, int64)")
+@njit('(float32[:,:], int64[:], int64, int64)')
 def process(S, P, a, b):
     signed_dist = cross2d(S[P] - S[a], S[b] - S[a])
-    K = np.array(
-        [i for s, i in zip(signed_dist, P) if s > 0 and i != a and i != b],
-        dtype=np.int64,
-    )
+    K = np.array([i for s, i in zip(signed_dist, P) if s > 0 and i != a and i != b], dtype=np.int64)
     if len(K) == 0:
         return [a, b]
     c = P[np.argmax(signed_dist)]
     return process(S, K, a, c)[:-1] + process(S, K, c, b)
 
-
-@njit("(float32[:, :,:], types.unicode_type)", fastmath=True)
-def jitted_hull(points: np.ndarray, target: str = "perimeter") -> np.ndarray:
+@njit('(float32[:, :,:], types.unicode_type)', fastmath=True)
+def jitted_hull(points: np.ndarray, target: str = 'perimeter') -> np.ndarray:
     """
     Compute attributes (e.g., perimeter or area) of a polygon.
 
     :param array points: 3d array FRAMESxBODY-PARTxCOORDINATE
-    :param str target: Options [perimeter, area]
+    :param str target: Options [perimeter, area, hull]
     :return: 1d np.array representing perimeter length or area of polygon on each frame
 
     .. note::
-    Modified from `Jérôme Richard <https://stackoverflow.com/questions/74812556/computing-quick-convex-hull-using-numba/74817179#74817179>`_
+       Modified from `Jérôme Richard <https://stackoverflow.com/questions/74812556/computing-quick-convex-hull-using-numba/74817179#74817179>`_
+
+    .. image:: _static/img/jitted_hull.png
+       :width: 400
+       :align: center
+
+
 
     :example:
     >>> points = np.random.randint(1, 50, size=(50, 5, 2)).astype(np.float32)
@@ -37,9 +38,9 @@ def jitted_hull(points: np.ndarray, target: str = "perimeter") -> np.ndarray:
 
     def perimeter(xy):
         perimeter = np.linalg.norm(xy[0] - xy[-1])
-        for i in prange(xy.shape[0] - 1):
-            p = np.linalg.norm(xy[i] - xy[i + 1])
-            perimeter += p
+        for i in prange(xy.shape[0]-1):
+            p = np.linalg.norm(xy[i] - xy[i+1])
+            perimeter+=p
         return perimeter
 
     def area(x, y):
@@ -48,29 +49,25 @@ def jitted_hull(points: np.ndarray, target: str = "perimeter") -> np.ndarray:
     results = np.full((points.shape[0]), np.nan)
     for i in range(points.shape[0]):
         S = points[i, :, :]
-        a, b = np.argmin(S[:, 0]), np.argmax(S[:, 0])
-        max_index = np.argmax(S[:, 0])
-        idx = (
-            process(S, np.arange(S.shape[0]), a, max_index)[:-1]
-            + process(S, np.arange(S.shape[0]), max_index, a)[:-1]
-        )
+        a, b = np.argmin(S[:,0]), np.argmax(S[:,0])
+        max_index = np.argmax(S[:,0])
+        idx = process(S, np.arange(S.shape[0]), a, max_index)[:-1] + process(S, np.arange(S.shape[0]), max_index, a)[:-1]
         x, y = np.full((len(idx)), np.nan), np.full((len(idx)), np.nan)
         for j in prange(len(idx)):
             x[j], y[j] = S[idx[j], 0], S[idx[j], 1]
         x0, y0 = np.mean(x), np.mean(y)
         r = np.sqrt((x - x0) ** 2 + (y - y0) ** 2)
-        angles = np.where(
-            (y - y0) > 0, np.arccos((x - x0) / r), 2 * np.pi - np.arccos((x - x0) / r)
-        )
+        angles = np.where((y - y0) > 0, np.arccos((x - x0) / r), 2 * np.pi - np.arccos((x - x0) / r))
         mask = np.argsort(angles)
         x_sorted, y_sorted = x[mask], y[mask]
-        if target == "perimeter":
+        if target == 'perimeter':
             xy = np.vstack((x_sorted, y_sorted)).T
             results[i] = perimeter(xy)
-        if target == "area":
+        if target == 'area':
             results[i] = area(x_sorted, y_sorted)
 
     return results
+
 
 
 @jit(nopython=True)
@@ -90,12 +87,9 @@ def jitted_centroid(points: np.ndarray) -> np.ndarray:
     results = np.full((points.shape[0], 2), np.nan)
     for i in range(points.shape[0]):
         S = points[i, :, :]
-        a, b = np.argmin(S[:, 0]), np.argmax(S[:, 0])
-        max_index = np.argmax(S[:, 0])
-        idx = (
-            process(S, np.arange(S.shape[0]), a, max_index)[:-1]
-            + process(S, np.arange(S.shape[0]), max_index, a)[:-1]
-        )
+        a, b = np.argmin(S[:,0]), np.argmax(S[:,0])
+        max_index = np.argmax(S[:,0])
+        idx = process(S, np.arange(S.shape[0]), a, max_index)[:-1] + process(S, np.arange(S.shape[0]), max_index, a)[:-1]
         perimeter_points = np.full((len(idx), 2), np.nan)
         for j in prange(len(idx)):
             perimeter_points[j] = points[i][j]
@@ -103,11 +97,12 @@ def jitted_centroid(points: np.ndarray) -> np.ndarray:
         results[i][1] = np.int(np.mean(perimeter_points[:, 1].flatten()))
     return results
 
-
 # points = np.random.randint(1, 5, size=(1, 10, 2)).astype(np.float32)
 # points[0][1] = np.nan
 # results = jitted_hull(points, target='area')
 # print(results)
+
+
 
 
 # points = np.random.randint(1, 10, size=(50, 5, 2)).astype(np.float32)
