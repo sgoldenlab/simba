bd889502d916faa816de1b56da56e268dee1ea58
Simon Nilsson <sronilsson@gmail.com>
2023-08-13 14:50:40 -0400

    Add files via upload

---
+++ simba/plotting/circular_feature_overlay_plotter.py
@@ -0,0 +1,141 @@
+import os
+from typing import Union
+import cv2
+import numpy as np
+
+from simba.mixins.plotting_mixin import PlottingMixin
+from simba.mixins.feature_extraction_mixin import FeatureExtractionMixin
+from simba.feature_extractors.perimeter_jit import jitted_hull
+from simba.mixins.config_reader import ConfigReader
+from simba.utils.read_write import find_video_of_file, get_fn_ext, get_video_meta_data, read_df
+from simba.utils.enums import Formats
+from simba.utils.checks import check_file_exist_and_readable, check_float, check_int
+from simba.utils.lookups import integer_to_cardinality_lookup
+from simba.utils.printing import stdout_success
+
+
+class CircularFeaturePlotter(ConfigReader,
+                             PlottingMixin,
+                             FeatureExtractionMixin):
+
+    """
+    Create visualization of base angular features overlay on video. E.g., use to confirm
+    accurate cardinality and angle degree computation.
+
+    .. image:: _static/img/base_angular_features.png
+       :width: 450
+       :align: center
+
+    :param Union[str, os.PathLike] config_path: path to SimBA project config file in Configparser format
+    :param Union[str, os.PathLike] data_path: Path to file containing angular features.
+    :param dict settings: Dictionary containing visualization attributes.
+
+    :example:
+    >>> settings = {'center': {'Animal_1': 'SwimBladder'}, 'text_settings': False, "palette": 'bwr'}
+    >>> circular_feature_plotter = CircularFeaturePlotter(config_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/project_config.ini', data_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/csv/circular_features/20200730_AB_7dpf_850nm_0002.csv', settings=settings)
+    >>> circular_feature_plotter.run()
+    """
+
+    def __init__(self,
+                 config_path: Union[str, os.PathLike],
+                 data_path: Union[str, os.PathLike],
+                 settings: dict):
+
+        PlottingMixin.__init__(self)
+        ConfigReader.__init__(self, config_path=config_path, read_video_info=True)
+        FeatureExtractionMixin.__init__(self)
+        self.video_path = find_video_of_file(video_dir=self.video_dir, filename=get_fn_ext(filepath=data_path)[1])
+        _, self.video_name, _ = get_fn_ext(filepath=self.video_path)
+        check_file_exist_and_readable(file_path=data_path)
+        self.data_path, self.config_path, self.settings, self.text_settings = data_path, config_path, settings, settings['text_settings']
+        self.save_path = os.path.join(self.project_path, 'frames', 'output', 'circular_features', get_fn_ext(filepath=data_path)[1] + '.mp4')
+        if not os.path.isdir(os.path.dirname(self.save_path)): os.makedirs(os.path.dirname(self.save_path))
+        self.fourcc = cv2.VideoWriter_fourcc(*Formats.MP4_CODEC.value)
+
+    def _approximate_size_of_animal_in_video(self):
+        self.animal_sizes = {}
+        for animal_name, animal_bps in self.animal_bp_dict.items():
+            self.animal_sizes[animal_name] = {}
+            animal_bp_cols = [item for pair in zip(animal_bps['X_bps'], animal_bps['Y_bps']) for item in pair]
+            animal_bp_data = self.df[animal_bp_cols].values.reshape(len(self.df), len(animal_bps['X_bps']), 2).astype(np.float32)
+            self.animal_sizes[animal_name]['area'] = np.nanmean(jitted_hull(points=animal_bp_data, target='area')).astype(np.int32)
+            self.animal_sizes[animal_name]['diameter'] = np.sqrt(self.animal_sizes[animal_name]['area'] / np.pi).astype(np.int32) * 3
+
+    def __get_print_settings(self):
+        if self.text_settings is False:
+            self.space_scale, self.radius_scale, self.res_scale, self.font_scale = 60, 12, 1500, 1.1
+            self.max_dim = max(self.video_meta_data['width'], self.video_meta_data['height'])
+            self.circle_scale = int(self.radius_scale / (self.res_scale / self.max_dim))
+            self.font_size = float(self.font_scale / (self.res_scale / self.max_dim))
+            self.spacing_scale = int(self.space_scale / (self.res_scale / self.max_dim))
+            self.text_thickness = 3
+        else:
+            check_float(name='ERROR: TEXT SIZE', value=self.text_settings['font_size'])
+            check_int(name='ERROR: SPACE SIZE', value=self.text_settings['space_size'])
+            check_int(name='ERROR: TEXT THICKNESS', value=self.text_settings['text_thickness'])
+            check_int(name='ERROR: CIRCLE SIZE', value=self.text_settings['circle_size'])
+            self.font_size = float(self.text_settings['font_size'])
+            self.spacing_scale = int(self.text_settings['space_size'])
+            self.text_thickness = int(self.text_settings['text_thickness'])
+            self.circle_scale = int(self.text_settings['circle_size'])
+
+    def __calc_text_locs(self):
+        add_spacer = 2
+        self.loc_dict = {}
+        for animal_cnt, animal_name in enumerate(self.multi_animal_id_list):
+            self.loc_dict[animal_name] = {}
+            self.loc_dict[animal_name]['degree_txt'] = f'{animal_name} angle:'
+            self.loc_dict[animal_name]['cardinal_txt'] = f'{animal_name} cardinal:'
+            self.loc_dict[animal_name]['degree_txt_loc'] = (5, (self.video_meta_data['height'] - (self.video_meta_data['height'] + 10) + self.spacing_scale * add_spacer))
+            self.loc_dict[animal_name]['degree_data_loc'] = (200, (self.video_meta_data['height'] - (self.video_meta_data['height'] + 10) + self.spacing_scale * add_spacer))
+            add_spacer += 1
+            self.loc_dict[animal_name]['cardinal_txt_loc'] = (5, (self.video_meta_data['height'] - (self.video_meta_data['height'] + 10) + self.spacing_scale * add_spacer))
+            self.loc_dict[animal_name]['cardinal_data_loc'] = (240, (self.video_meta_data['height'] - (self.video_meta_data['height'] + 10) + self.spacing_scale * add_spacer))
+            add_spacer += 1
+
+    def _create_palette(self):
+        c = self.create_single_color_lst(pallete_name=self.settings['palette'], increments=360)
+        self.colors = {}
+        for color_cnt, color in enumerate(c): self.colors[color_cnt] = color
+
+    def run(self):
+        self.df = read_df(file_path=self.data_path, file_type=self.file_type)
+        self.video_meta_data, frm_cnt = get_video_meta_data(video_path=self.video_path), 0
+        self.__get_print_settings()
+        self.cap = cv2.VideoCapture(self.video_path)
+        self.writer = cv2.VideoWriter(self.save_path, self.fourcc, self.video_meta_data['fps'], (self.video_meta_data['width'], self.video_meta_data['height']))
+        self._approximate_size_of_animal_in_video()
+        self._create_palette()
+        self.__calc_text_locs()
+        for animal_name, animal_bps in self.animal_bp_dict.items():
+            self.df[f'Compass_cardinal_{animal_name}'] = self.df[f'Compass_cardinal_{animal_name}'].map(integer_to_cardinality_lookup())
+        while (self.cap.isOpened()):
+            ret, self.frame = self.cap.read()
+            if not ret:
+                break
+            frm_data = self.df.iloc[frm_cnt, :]
+            for animal_name, animal_bps in self.animal_bp_dict.items():
+                compass_center = tuple(frm_data[[f'{self.settings["center"][animal_name]}_x', f'{self.settings["center"][animal_name]}_y']].values.astype(int))
+                animal_frm_angle, compass_cardinal = int(frm_data[f'Angle_degrees_{animal_name}']), frm_data[f'Compass_cardinal_{animal_name}']
+                frm_compass_clr = self.colors[animal_frm_angle]
+                cv2.circle(self.frame, compass_center, self.animal_sizes[animal_name]['diameter'], frm_compass_clr, self.text_thickness)
+                cv2.putText(self.frame, self.loc_dict[animal_name]['degree_txt'], self.loc_dict[animal_name]['degree_txt_loc'], self.font, self.font_size, frm_compass_clr, 1)
+                cv2.putText(self.frame, str(animal_frm_angle), self.loc_dict[animal_name]['degree_data_loc'], self.font, self.font_size, frm_compass_clr, 1)
+                cv2.putText(self.frame, self.loc_dict[animal_name]['cardinal_txt'], self.loc_dict[animal_name]['cardinal_txt_loc'], self.font, self.font_size, frm_compass_clr, 1)
+                cv2.putText(self.frame, compass_cardinal, self.loc_dict[animal_name]['cardinal_data_loc'], self.font, self.font_size, frm_compass_clr, 1)
+
+            self.writer.write(self.frame)
+            print(f'Image {frm_cnt+1}/{self.video_meta_data["frame_count"]} Video: {self.video_path}')
+            frm_cnt += 1
+        self.timer.stop_timer()
+        self.writer.release()
+        stdout_success(msg=f'Video {self.save_path} complete!', elapsed_time=self.timer.elapsed_time_str)
+
+
+# settings = {'center': {'Animal_1': 'SwimBladder'},
+#             'text_settings': False, "palette": 'bwr'}
+#
+# test = CircularFeaturePlotter(config_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/project_config.ini',
+#                               data_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/csv/circular_features/20200730_AB_7dpf_850nm_0002.csv',
+#                               settings=settings)
+# test.run()
---
+++ simba/plotting/circular_plotting.py
@@ -0,0 +1,187 @@
+import os
+from typing import Union, Optional
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pyplot import figure, cm
+from simba.mixins.plotting_mixin import PlottingMixin
+from simba.utils.printing import stdout_success
+
+class CircularPlotting(PlottingMixin):
+
+    def __init__(self):
+        PlottingMixin.__init__(self)
+
+    def diffusion_plot(self,
+                       data: np.ndarray,
+                       fps: int,
+                       degree_width: int,
+                       palette: str,
+                       title: Optional[str] = None,
+                       save_path: Optional[Union[str, os.PathLike]] = None) -> plt.figure:
+        """
+        Create polar plot representing the angular diffusion within a video.
+
+        .. image:: _static/img/circular_plotter.png
+          :width: 450
+          :align: center
+
+        :param np.ndarray data: 1D np.ndarray with angle in degrees with one entry per frame.
+        :param int fps: Framerate the video was recorded in.
+        :param int degree_width: The width of the bars in the plot.
+        :param str palette: The polar plot palette.
+        :param str title: Title of the plot
+        :param Optional[Union[str, os.PathLike]] save_path: Plot save location on disk. If None, then return plt.figure polar plot.
+
+        :example:
+        >>> data = np.random.normal(loc=180, scale=99, size=5000)
+        >>> _ = CircularPlotting().diffusion_plot(data=data, title='Mean 180 degree plot', fps=30, degree_width=5, palette='jet', save_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/frames/output/dispersion/20200730_AB_7dpf_850nm_0004.png')
+        """
+
+        max_seconds = int(data.shape[0] / fps)
+        second_bin =  int(max_seconds / 5)
+        if second_bin > 1: second_bin = 1
+        data_rad = [x * 2 * np.pi / 360 for x in data]
+        angle_bin_starts = np.arange(0.0, 2 * np.pi, 2 * np.pi * (degree_width / 360))
+        n_length_bins = int(max_seconds / second_bin)
+        bin_width = 2 * np.pi * (degree_width / 360)
+        counts, bin_edges = np.histogram(data_rad, bins=angle_bin_starts)
+        colors = self.create_single_color_lst(pallete_name=palette, increments=bin_edges.shape[0], as_rgb_ratio=True)
+        norm_counts = counts / (fps * second_bin)
+        bin_numbers = [np.round(norm_counts * n_length_bins / max_seconds, 0)]
+        bin_lengths = [x / degree_width for x in bin_numbers]
+
+        plt.figure().clear()
+        plt.close()
+        fig = figure(figsize=(8,8))
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
+        bars = ax.bar(angle_bin_starts[:-1], bin_lengths[0], width=bin_width, bottom=0.0)
+        ax.set_theta_zero_location('N')
+        ax.set_theta_direction(-1)
+        ax.set_yticklabels([])
+        for cnt, (r, bar) in enumerate(zip(bin_lengths[0], bars)):
+           bar.set_facecolor(colors[cnt])
+        if title is not None:
+            plt.title(title)
+        if save_path is not None:
+            plt.savefig(save_path)
+            stdout_success(msg=f'Diffusion plot {save_path} created!')
+
+        return ax
+
+    def diffusion_time_bin_plot(self,
+                                data: np.ndarray,
+                                fps: int,
+                                time_bin: int,
+                                degree_width: int,
+                                palette: str,
+                                save_path: Union[str, os.PathLike]):
+
+        """
+        Create polar plots representing angular diffusion within each N second time-bin of the video.
+
+        .. image:: _static/img/cicular_time_bins.png
+          :width: 600
+          :align: center
+
+
+        :param np.ndarray data: 1D np.ndarray with angle in degrees with one entry per frame.
+        :param int fps: Framerate the video was recorded in.
+        :param int time_bin: The length of each time bin (one plot will be created per time bin).
+        :param int degree_width: The width of the bars in the plot.
+        :param str palette: The polar plot palette.
+        :param Optional[Union[str, os.PathLike]] save_path: Plot save location on disk. If None, then return plt.figure polar plot.
+
+        :example:
+        >>> data = np.random.normal(loc=180, scale=99, size=5000)
+        >>> _ = CircularPlotting().diffusion_time_bin_plot(data=data, fps=30, degree_width=40, palette='jet', save_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/frames/output/dispertion_time_series/20200730_AB_7dpf_850nm_0004', time_bin=10)
+        """
+        time_bin_frame_size = time_bin * fps
+        split_data = np.array_split(data, data.shape[0] / time_bin_frame_size)
+        if not os.path.isdir(save_path): os.makedirs(save_path)
+        for bin_cnt, time_bin_data in enumerate(split_data):
+            time_plot = self.diffusion_plot(data=time_bin_data, fps=fps, degree_width=degree_width, palette=palette, title=f'Time bin {bin_cnt+1}')
+            fig_save_path = os.path.join(save_path, f'Time_bin_{bin_cnt+1}.png')
+            time_plot.figure.savefig(fig_save_path)
+            plt.figure().clear()
+            plt.close()
+            plt.clf()
+            stdout_success(msg=f'Diffusion plot {save_path} created!')
+
+
+
+#
+#
+#         #data = np.split(data, data.shape[0])
+#
+#
+#
+#
+# #data = read_df('/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/csv/circular_features/20200730_AB_7dpf_850nm_0004.csv', file_type='csv', usecols=[f'Angle_degrees_Animal_1']).values.flatten()
+#
+# data = np.random.normal(loc=90, scale=99, size=5000)
+#
+#
+# _ = CircularPlotting().diffusion_plot(data=data, title='test', fps=30, degree_width=20, palette='jet', save_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/frames/output/dispersion/20200730_AB_7dpf_850nm_0004.png')
+#
+# #_ = CircularPlotting().diffusion_time_bin_plot(data=data, fps=30, degree_width=40, palette='jet', save_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/frames/output/dispertion_time_series/20200730_AB_7dpf_850nm_0004', time_bin=10)
+#
+#
+# print(np.rad2deg(circstd(np.deg2rad(data))))
+# print(np.rad2deg(circmean(np.deg2rad(data))))
+#
+#
+# config_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/project_config.ini'
+# save_path='/Users/simon/Desktop/envs/troubleshooting/circular_features_zebrafish/project_folder/frames/output/20200730_AB_7dpf_850nm_0002.png'
+# DW = 20
+# MAX_SECONDS = 10
+# SECOND_BIN = 1
+# LAST_BIN = 2*np.pi
+#
+# length_raw = np.arange(0,MAX_SECONDS+SECOND_BIN, SECOND_BIN)
+# length_dict = {x: x/MAX_SECONDS for x in length_raw}
+# data_rad = [x*2*np.pi/360 for x in data]
+# angle_bin_starts = np.arange(0.0, 2*np.pi, 2*np.pi * (DW/360))
+# n_length_bins = int(MAX_SECONDS/SECOND_BIN)
+# bin_width = 2*np.pi * (DW/360)
+#
+# # for bin_start in angle_bin_starts:
+# counts, bin_edges = np.histogram(data_rad, bins=angle_bin_starts)
+# colors = PlottingMixin().create_single_color_lst(pallete_name='jet', increments=bin_edges.shape[0], as_rgb_ratio=True)
+# norm_counts = counts/(30*SECOND_BIN)
+# bin_numbers = [np.round(norm_counts * n_length_bins/MAX_SECONDS, 0)]
+# bin_lengths = [x/5 for x in bin_numbers]
+# bin_widths = [bin_width for idx in range(0, 72)]
+# fig = figure(figsize=(8,8))
+# ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
+# #ax.set_yticklabels([list(range(1, MAX_SECONDS))])
+# bars = ax.bar(angle_bin_starts[:-1], bin_lengths[0], width=bin_width, bottom=0.0)
+# ax.set_theta_zero_location('N')
+# ax.set_theta_direction(-1)
+# ax.set_yticklabels([])
+# #labels = [str(int(x)) + 's' for x in np.linspace(0, MAX_SECONDS, 4)]
+# #labels_cnt = np.linspace(0, 1, 4)
+#
+# #ax.set_rgrids(labels_cnt, labels, fontsize=16)
+# #lines, labels = ax.set_rgrids(labels_cnt, labels, fontsize=16)
+# for cnt, (r, bar) in enumerate(zip(bin_lengths[0], bars)):
+#     bar.set_facecolor(colors[cnt])
+#     #bar.set_facecolor([cm.Reds(r)])
+# #
+# #
+# #
+#
+# # #plt.clf()
+# # compass_plot = plt.subplot(1, 1, 1, projection='polar')
+# # compass_plot.set_theta_zero_location('N')
+# # bars = compass_plot.bar(data, 0.01, width=0.01, bottom=0.0)
+# #
+# # # N = 20
+# # theta = np.arange(0.0, 2*np.pi, 2*np.pi/N)
+# # radii = 10*np.random.rand(N)
+# # width = np.pi/4*np.random.rand(N)
+# # bars = ax.bar(theta, radii, width=width, bottom=0.0)
+# # for r,bar in zip(radii, bars):
+# #     bar.set_facecolor( cm.jet(r/10.))
+# #     bar.set_alpha(0.5)
+# #
+# # show()
