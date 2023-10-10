3fa061b60901ce762b535289e9867b2344ac8ae0
Simon Nilsson <sronilsson@gmail.com>
2023-10-10 16:17:10 -0400

    Update train_model_mixin.py

--- simba/mixins/train_model_mixin.py
+++ simba/mixins/train_model_mixin.py
@@ -772,7 +772,8 @@ class TrainModelMixin(object):
 
 
         .. math::
-           :nowrap:
+           :align: center
+
            \phi_i(f, x) = \\sum_{S \\subseteq F \\setminus {i}} \\frac{|S|!(|F| - |S| - 1)!}{|F|!} (f_{S \cup {i}}(x_{S \\cup {i}}) - f_S(x_S))
 
         :param str ini_file_path: Path to the SimBA project_config.ini
