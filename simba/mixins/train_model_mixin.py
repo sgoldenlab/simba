a40b79764a3bd1c5456273bc344fec5cfe6a9b12
Simon Nilsson <sronilsson@gmail.com>
2023-10-10 14:42:56 -0400

    Update train_model_mixin.py

--- simba/mixins/train_model_mixin.py
+++ simba/mixins/train_model_mixin.py
@@ -771,9 +771,7 @@ class TrainModelMixin(object):
            For improved run-times, use multiprocessing through :meth:`simba.mixins.train_model_mixins.TrainModelMixin.create_shap_log_mp`
 
         .. math::
-           \begin{equation}
-           \text{Mobility} = \sqrt{\frac{\text{variance of the first derivative}}{\text{variance of the signal}}}
-           \end{equation}
+           \phi_i(f, x) = \sum_{S \subseteq F \setminus {i}} \frac{|S|!(|F| - |S| - 1)!}{|F|!} (f_{S \cup {i}}(x_{S \cup {i}}) - f_S(x_S))
 
         :param str ini_file_path: Path to the SimBA project_config.ini
         :param RandomForestClassifier rf_clf: sklearn random forest classifier
