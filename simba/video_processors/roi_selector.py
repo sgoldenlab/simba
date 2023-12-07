4ee7f7a397b326f75e5e38a8f5a2214ba970c820
sronilsson <sronilsson@users.noreply.github.com>
2023-12-07 21:15:54 +0000

    cleaned

--- simba/video_processors/roi_selector.py
+++ simba/video_processors/roi_selector.py
@@ -1,17 +1,18 @@
+import os
+from typing import Optional, Tuple, Union
+
 import cv2
 import numpy as np
-from typing import Union, Tuple, Optional
-import os
+
 from simba.utils.checks import (check_file_exist_and_readable,
-                                check_if_valid_rgb_tuple,
-                                check_int,
-                                check_str)
-from simba.utils.warnings import CropWarning
-from simba.utils.read_write import get_fn_ext, get_video_meta_data
+                                check_if_valid_rgb_tuple, check_int, check_str)
 from simba.utils.enums import Options
 from simba.utils.errors import InvalidFileTypeError
+from simba.utils.read_write import get_fn_ext, get_video_meta_data
+from simba.utils.warnings import CropWarning
 
-class ROISelector():
+
+class ROISelector:
     """
     A class for selecting and reflecting Regions of Interest (ROI) in an image.
     The selected region variables:  top_left, bottom_right, width, height are stored in self.
@@ -26,23 +27,23 @@ class ROISelector():
     >>> img_selector.run()
     """
 
-
-    def __init__(self,
-                 path: Union[str, os.PathLike],
-                 thickness: int = 10,
-                 clr: Tuple[int, int, int] = (147, 20, 255),
-                 title: Optional[str] = None) -> None:
-
+    def __init__(
+        self,
+        path: Union[str, os.PathLike],
+        thickness: int = 10,
+        clr: Tuple[int, int, int] = (147, 20, 255),
+        title: Optional[str] = None,
+    ) -> None:
         check_file_exist_and_readable(file_path=path)
         check_if_valid_rgb_tuple(data=clr)
-        check_int(name='Thickness', value=thickness, min_value=1, raise_error=True)
-        if title is not None: check_str(name='ROI title window', value=title)
+        check_int(name="Thickness", value=thickness, min_value=1, raise_error=True)
+        if title is not None:
+            check_str(name="ROI title window", value=title)
         self.roi_start = None
         self.roi_end = None
         self.selecting_roi = False
         self.clr = clr
         self.thickness = int(thickness)
-
 
         if isinstance(path, np.ndarray):
             self.image = path
@@ -58,14 +59,16 @@ class ROISelector():
                 self.image = cv2.imread(path).astype(np.uint8)
 
             else:
-                raise InvalidFileTypeError(msg=f'Cannot crop a {ext} file.', source=self.__class__.__name__)
+                raise InvalidFileTypeError(
+                    msg=f"Cannot crop a {ext} file.", source=self.__class__.__name__
+                )
 
-            title = f'Draw ROI video {filename} - Press ESC when drawn'
+            title = f"Draw ROI video {filename} - Press ESC when drawn"
 
         self.img_cpy = self.image.copy()
 
         if title is None:
-            self.title = 'Draw ROI - Press ESC when drawn'
+            self.title = "Draw ROI - Press ESC when drawn"
         else:
             self.title = title
 
@@ -82,8 +85,9 @@ class ROISelector():
         elif event == cv2.EVENT_MOUSEMOVE and self.selecting_roi:
             self.roi_end = (x, y)
             self.img_cpy = self.image.copy()
-            cv2.rectangle(self.img_cpy, self.roi_start, self.roi_end, self.clr, self.thickness)
-
+            cv2.rectangle(
+                self.img_cpy, self.roi_start, self.roi_end, self.clr, self.thickness
+            )
 
     def run(self):
         cv2.namedWindow(self.title, cv2.WINDOW_NORMAL)
@@ -93,15 +97,31 @@ class ROISelector():
             if self.selecting_roi or self.img_cpy is None:
                 self.img_cpy = self.image.copy()
                 if self.roi_start is not None and self.roi_end is not None:
-                    cv2.rectangle(self.img_cpy, self.roi_start, self.roi_end, self.clr, self.thickness)
+                    cv2.rectangle(
+                        self.img_cpy,
+                        self.roi_start,
+                        self.roi_end,
+                        self.clr,
+                        self.thickness,
+                    )
 
             cv2.imshow(self.title, self.img_cpy)
             key = cv2.waitKey(1) & 0xFF
 
-            if key == ord('c') and self.roi_start is not None and self.roi_end is not None:
-                selected_roi = self.image[self.roi_start[1]:self.roi_end[1], self.roi_start[0]:self.roi_end[0]].copy()
+            if (
+                key == ord("c")
+                and self.roi_start is not None
+                and self.roi_end is not None
+            ):
+                selected_roi = self.image[
+                    self.roi_start[1] : self.roi_end[1],
+                    self.roi_start[0] : self.roi_end[0],
+                ].copy()
                 reflected_roi = cv2.flip(selected_roi, 1)
-                self.image[self.roi_start[1]:self.roi_end[1], self.roi_start[0]:self.roi_end[0]] = reflected_roi
+                self.image[
+                    self.roi_start[1] : self.roi_end[1],
+                    self.roi_start[0] : self.roi_end[0],
+                ] = reflected_roi
                 self.roi_start = None
                 self.roi_end = None
 
@@ -112,25 +132,33 @@ class ROISelector():
                     break
 
     def run_checks(self):
-        self.top_left = min(self.roi_start[0], self.roi_end[0]), min(self.roi_start[1],self.roi_end[1])
-        self.bottom_right = max(self.roi_start[0], self.roi_end[0]), max(self.roi_start[1], self.roi_end[1])
+        self.top_left = min(self.roi_start[0], self.roi_end[0]), min(
+            self.roi_start[1], self.roi_end[1]
+        )
+        self.bottom_right = max(self.roi_start[0], self.roi_end[0]), max(
+            self.roi_start[1], self.roi_end[1]
+        )
         self.width = self.bottom_right[0] - self.top_left[0]
         self.height = self.bottom_right[1] - self.top_left[1]
 
-        if (self.width == 0 and self.height == 0) or (self.width + self.height + self.top_left[0] + self.top_left[1] == 0):
-            CropWarning(msg='CROP WARNING: Cropping height and width are both 0. Please try again.', source=self.__class__.__name__)
+        if (self.width == 0 and self.height == 0) or (
+            self.width + self.height + self.top_left[0] + self.top_left[1] == 0
+        ):
+            CropWarning(
+                msg="CROP WARNING: Cropping height and width are both 0. Please try again.",
+                source=self.__class__.__name__,
+            )
             return False
 
         else:
             return True
 
 
+# img_selector = ROISelector(path='/Users/simon/Desktop/envs/troubleshooting/ARES_data/Termite Test 1/Termite Test 1.mp4')
+# img_selector.run()
 
-#img_selector = ROISelector(path='/Users/simon/Desktop/envs/troubleshooting/ARES_data/Termite Test 1/Termite Test 1.mp4')
-#img_selector.run()
-
-#img_selector = ROISelector(path='/Users/simon/Desktop/compute_overlap.png', clr=(0, 255, 0), thickness=2)
-#img_selector.run()
+# img_selector = ROISelector(path='/Users/simon/Desktop/compute_overlap.png', clr=(0, 255, 0), thickness=2)
+# img_selector.run()
 
 #
 # # Global variables to store the ROI coordinates
