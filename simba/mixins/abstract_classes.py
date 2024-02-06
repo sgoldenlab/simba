415dc903726fec94b64ad0ebe2df8e0e4fe44cd5
Simon Nilsson <sronilsson@gmail.com>
2024-02-06 16:19:27 -0500

    Add files via upload

--- simba/mixins/abstract_classes.py
+++ simba/mixins/abstract_classes.py
@@ -1,13 +1,14 @@
-import os
 from abc import ABC, abstractmethod
-from typing import Union
 
 import pandas as pd
-
+from typing import Union
+import os
 
 class AbstractFeatureExtraction(ABC):
+
     @abstractmethod
-    def __init__(self, config_path: Union[str, os.PathLike]):
+    def __init__(self,
+                 config_path: Union[str, os.PathLike]):
         pass
 
     @abstractmethod
@@ -15,5 +16,7 @@ class AbstractFeatureExtraction(ABC):
         pass
 
     @abstractmethod
-    def save(self, data: pd.DataFrame, save_path: Union[str, os.PathLike]):
+    def save(self,
+             data: pd.DataFrame,
+             save_path: Union[str, os.PathLike]):
         pass
--- simba/mixins/unsupervised_mixin.py
+++ simba/mixins/unsupervised_mixin.py
@@ -1,51 +1,50 @@
 __author__ = "Simon Nilsson"
 
-import glob
-import os
+import os, glob
 import pickle
-from datetime import datetime
-from typing import List, Optional, Union
-
-import numpy as np
-import pandas as pd
-from sklearn.feature_selection import VarianceThreshold
-
 import simba
-
+from sklearn.feature_selection import VarianceThreshold
+import pandas as pd
+from datetime import datetime
+import numpy as np
+from typing import Union, List, Optional
 try:
     from typing import Literal
 except:
     from typing_extensions import Literal
 
-from sklearn.preprocessing import (MinMaxScaler, QuantileTransformer,
-                                   StandardScaler)
-
-from simba.unsupervised.enums import UMLOptions, Unsupervised
+from sklearn.preprocessing import (MinMaxScaler,
+                                   StandardScaler,
+                                   QuantileTransformer)
+from simba.utils.errors import (InvalidInputError,
+                                NoDataError,
+                                NoFilesFoundError,
+                                InvalidFileTypeError,
+                                MissingColumnsError,
+                                IntegerError,
+                                DirectoryNotEmptyError)
 from simba.utils.checks import check_float
-from simba.utils.enums import Paths
-from simba.utils.errors import (DirectoryNotEmptyError, IntegerError,
-                                InvalidFileTypeError, InvalidInputError,
-                                MissingColumnsError, NoDataError,
-                                NoFilesFoundError)
 from simba.utils.printing import SimbaTimer
+from simba.unsupervised.enums import Unsupervised, UMLOptions
+from simba.utils.enums import Paths
 
 
 class UnsupervisedMixin(object):
     """
     Methods for unsupervised ML.
     """
-
     def __init__(self):
-        self.datetime = datetime.now().strftime("%Y%m%d%H%M%S")
-        self.timer = SimbaTimer(start=True)
-        model_names_dir = os.path.join(
-            os.path.dirname(simba.__file__), Paths.UNSUPERVISED_MODEL_NAMES.value
-        )
-        self.model_names = list(
-            pd.read_parquet(model_names_dir)[Unsupervised.NAMES.value]
-        )
 
-    def read_pickle(self, data_path: Union[str, os.PathLike]) -> dict:
+
+
+        self.datetime = datetime.now().strftime('%Y%m%d%H%M%S')
+        self.timer = SimbaTimer(start=True)
+        model_names_dir = os.path.join(os.path.dirname(simba.__file__), Paths.UNSUPERVISED_MODEL_NAMES.value)
+        self.model_names = list(pd.read_parquet(model_names_dir)[Unsupervised.NAMES.value])
+
+    def read_pickle(self,
+                    data_path: Union[str, os.PathLike]) -> dict:
+
         """
         Read a single or directory of pickled objects. If directory, returns dict with numerical sequential integer keys for
         each object.
@@ -59,20 +58,20 @@ class UnsupervisedMixin(object):
 
         if os.path.isdir(data_path):
             data = {}
-            files_found = glob.glob(data_path + "/*.pickle")
+            files_found = glob.glob(data_path + '/*.pickle')
             if len(files_found) == 0:
-                raise NoFilesFoundError(
-                    msg=f"SIMBA ERROR: Zero pickle files found in {data_path}."
-                )
+                raise NoFilesFoundError(msg=f'SIMBA ERROR: Zero pickle files found in {data_path}.', source=self.__class__.__name__)
             for file_cnt, file_path in enumerate(files_found):
-                with open(file_path, "rb") as f:
+                with open(file_path, 'rb') as f:
                     data[file_cnt] = pickle.load(f)
         if os.path.isfile(data_path):
-            with open(data_path, "rb") as f:
+            with open(data_path, 'rb') as f:
                 data = pickle.load(f)
         return data
 
-    def write_pickle(self, data: dict, save_path: Union[str, os.PathLike]) -> None:
+    def write_pickle(self,
+                     data: dict,
+                     save_path: Union[str, os.PathLike]) -> None:
         """
         Write a single object as pickle.
 
@@ -84,13 +83,15 @@ class UnsupervisedMixin(object):
         """
 
         try:
-            with open(save_path, "wb") as f:
+            with open(save_path, 'wb') as f:
                 pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
         except Exception as e:
             print(e.args[0])
-            raise InvalidFileTypeError(msg="Data could not be saved as a pickle.")
+            raise InvalidFileTypeError(msg='Data could not be saved as a pickle.', source=self.__class__.__name__)
 
-    def check_umap_hyperparameters(self, hyper_parameters: dict):
+
+    def check_umap_hyperparameters(self,
+                                   hyper_parameters: dict):
         """
         Checks if umap embedder hyperparameters are valid.
 
@@ -103,32 +104,15 @@ class UnsupervisedMixin(object):
 
         for key in Unsupervised.HYPERPARAMETERS.value:
             if key not in hyper_parameters.keys():
-                raise InvalidInputError(
-                    msg=f"Hyperparameter dictionary is missing {key} entry."
-                )
-        for key in [
-            Unsupervised.N_NEIGHBORS.value,
-            Unsupervised.MIN_DISTANCE.value,
-            Unsupervised.SPREAD.value,
-        ]:
+                raise InvalidInputError(msg=f'Hyperparameter dictionary is missing {key} entry.', source=self.__class__.__name__)
+        for key in [Unsupervised.N_NEIGHBORS.value, Unsupervised.MIN_DISTANCE.value, Unsupervised.SPREAD.value]:
             if len(hyper_parameters[key]) == 0:
-                raise InvalidInputError(
-                    msg=f"Hyperparameter dictionary key {key} has 0 entries."
-                )
-        if (
-            hyper_parameters[Unsupervised.SCALER.value]
-            not in UMLOptions.SCALER_OPTIONS.value
-        ):
-            raise InvalidInputError(
-                msg=f"Scaler {Unsupervised.SCALER.value} not supported. Opitions: {UMLOptions.SCALER_OPTIONS.value}"
-            )
-        check_float(
-            "VARIANCE THRESHOLD", value=hyper_parameters[Unsupervised.VARIANCE.value]
-        )
+                raise InvalidInputError(msg=f'Hyperparameter dictionary key {key} has 0 entries.', source=self.__class__.__name__)
+        if hyper_parameters[Unsupervised.SCALER.value] not in UMLOptions.SCALER_OPTIONS.value:
+            raise InvalidInputError(msg=f'Scaler {Unsupervised.SCALER.value} not supported. Opitions: {UMLOptions.SCALER_OPTIONS.value}', source=self.__class__.__name__)
+        check_float('VARIANCE THRESHOLD', value=hyper_parameters[Unsupervised.VARIANCE.value])
 
-    def find_low_variance_fields(
-        self, data: pd.DataFrame, variance: float
-    ) -> List[str]:
+    def find_low_variance_fields(self, data: pd.DataFrame, variance: float) -> List[str]:
         """
         Finds fields with variance below provided threshold.
 
@@ -138,15 +122,9 @@ class UnsupervisedMixin(object):
         """
         feature_selector = VarianceThreshold(threshold=round((variance / 100), 2))
         feature_selector.fit(data)
-        low_variance_fields = [
-            c
-            for c in data.columns
-            if c not in data.columns[feature_selector.get_support()]
-        ]
+        low_variance_fields = [c for c in data.columns if c not in data.columns[feature_selector.get_support()]]
         if len(low_variance_fields) == len(data.columns):
-            raise NoDataError(
-                msg=f"All feature columns show a variance below the {str(variance)} threshold. Thus, no data remain for analysis."
-            )
+            raise NoDataError(msg=f'All feature columns show a variance below the {str(variance)} threshold. Thus, no data remain for analysis.', source=self.find_low_variance_fields.__name__)
         return low_variance_fields
 
     def drop_fields(self, data: pd.DataFrame, fields: List[str]) -> pd.DataFrame:
@@ -158,9 +136,7 @@ class UnsupervisedMixin(object):
         """
         return data.drop(columns=fields)
 
-    def define_scaler(
-        self, scaler_name: Literal["MIN-MAX", "STANDARD", "QUANTILE"]
-    ) -> Union[MinMaxScaler, StandardScaler, QuantileTransformer]:
+    def define_scaler(self, scaler_name: Literal['MIN-MAX', 'STANDARD', 'QUANTILE']) -> Union[MinMaxScaler, StandardScaler, QuantileTransformer]:
         """
         Creates sklearn scaler object. See ``UMLOptions.SCALER_OPTIONS.value`` for accepted scalers.
 
@@ -169,9 +145,7 @@ class UnsupervisedMixin(object):
         """
 
         if scaler_name not in UMLOptions.SCALER_OPTIONS.value:
-            raise InvalidInputError(
-                msg=f"Scaler {scaler_name} not supported. Options: {UMLOptions.SCALER_OPTIONS.value}"
-            )
+            raise InvalidInputError(msg=f'Scaler {scaler_name} not supported. Options: {UMLOptions.SCALER_OPTIONS.value}', source=self.__class__.__name__)
         if scaler_name == Unsupervised.MIN_MAX.value:
             return MinMaxScaler()
         elif scaler_name == Unsupervised.STANDARD.value:
@@ -179,45 +153,34 @@ class UnsupervisedMixin(object):
         elif scaler_name == Unsupervised.QUANTILE.value:
             return QuantileTransformer()
 
-    def scaler_transform(
-        self,
-        data: pd.DataFrame,
-        scaler: Literal[MinMaxScaler or StandardScaler or QuantileTransformer],
-    ) -> pd.DataFrame:
+    def scaler_transform(self,
+                         data: pd.DataFrame,
+                         scaler: Literal[MinMaxScaler or StandardScaler or QuantileTransformer]) -> pd.DataFrame:
         """
         Helper to run transform using fitted scaler.
-
+        
         :param pd.DataFrame data: Data to transform.
         :param scaler: fitted scaler.
         """
 
-        return pd.DataFrame(scaler.transform(data), columns=data.columns).set_index(
-            data.index
-        )
+        return pd.DataFrame(scaler.transform(data), columns=data.columns).set_index(data.index)
 
-    def scaler_inverse_transform(
-        self,
-        data: pd.DataFrame,
-        scaler=MinMaxScaler or StandardScaler or QuantileTransformer,
-    ):
-        return pd.DataFrame(
-            scaler.inverse_transform(data), columns=data.columns
-        ).set_index(data.index)
+    def scaler_inverse_transform(self, data: pd.DataFrame, scaler=MinMaxScaler or StandardScaler or QuantileTransformer):
+        return pd.DataFrame(scaler.inverse_transform(data), columns=data.columns).set_index(data.index)
 
     def check_expected_fields(self, data_fields: list, expected_fields: list):
         remaining_fields = [x for x in data_fields if x not in expected_fields]
         if len(remaining_fields) > 0:
-            raise MissingColumnsError(
-                f"The data contains {str(len(remaining_fields))} unexpected field(s): {str(remaining_fields)}"
-            )
+            raise MissingColumnsError(f'The data contains {str(len(remaining_fields))} unexpected field(s): {str(remaining_fields)}', source=self.__class__.__name__)
 
     def check_key_exist_in_object(self, object: dict, key: str):
         if key not in object.keys():
-            raise InvalidInputError(msg=f"{key} does not exist in object")
+            raise InvalidInputError(msg=f'{key} does not exist in object', source=self.__class__.__name__)
 
-    def get_cluster_cnt(
-        self, data: np.array, clusterer_name: str, minimum_clusters: Optional[int] = 1
-    ) -> int:
+    def get_cluster_cnt(self,
+                        data: np.array,
+                        clusterer_name: str,
+                        minimum_clusters: Optional[int] = 1) -> int:
         """
         Helper to check the number of unique observations in array. E.g., check the number of unique clusters.
 
@@ -229,9 +192,7 @@ class UnsupervisedMixin(object):
 
         cnt = np.unique(data).shape[0]
         if cnt < minimum_clusters:
-            raise IntegerError(
-                msg=f"Clustrer {clusterer_name} has {str(cnt)} clusters, but {str(minimum_clusters)} clusters is required for the operation."
-            )
+            raise IntegerError(msg=f'Clustrer {clusterer_name} has {str(cnt)} clusters, but {str(minimum_clusters)} clusters is required for the operation.', source=self.__class__.__name__)
         return cnt
 
     def check_that_directory_is_empty(self, directory: Union[str, os.PathLike]) -> None:
@@ -243,13 +204,9 @@ class UnsupervisedMixin(object):
         """
 
         try:
-            all_files_in_folder = [
-                f for f in next(os.walk(directory))[2] if not f[0] == "."
-            ]
+            all_files_in_folder = [f for f in next(os.walk(directory))[2] if not f[0] == '.']
         except StopIteration:
             return 0
         else:
             if len(all_files_in_folder) > 0:
-                raise DirectoryNotEmptyError(
-                    msg=f"The {directory} is not empty and contains {str(len(all_files_in_folder))} files. Use a directory that is empty."
-                )
+                raise DirectoryNotEmptyError(msg=f'The {directory} is not empty and contains {str(len(all_files_in_folder))} files. Use a directory that is empty.', source=self.__class__.__name__)
