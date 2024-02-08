1f0cbb65f0bb70cfe85c20e1b66ac8fedc057204
sronilsson <sronilsson@users.noreply.github.com>
2024-02-08 02:12:36 +0000

    cleaned

--- simba/sandbox/geometry_ex.py
+++ simba/sandbox/geometry_ex.py
@@ -4,6 +4,8 @@ from copy import deepcopy
 from typing import Dict, Optional, Union
 
 import cv2
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -17,10 +19,10 @@ from simba.utils.checks import check_instance, check_int
 from simba.utils.enums import Formats
 from simba.utils.errors import NoFilesFoundError
 from simba.utils.printing import SimbaTimer, stdout_success
-from simba.utils.read_write import (get_video_meta_data, read_df, read_frm_of_video)
-import matplotlib.pyplot as plt
-import matplotlib
-dpi = matplotlib.rcParams['figure.dpi']
+from simba.utils.read_write import (get_video_meta_data, read_df,
+                                    read_frm_of_video)
+
+dpi = matplotlib.rcParams["figure.dpi"]
 
 CONFIG_PATH = "/Users/simon/Desktop/envs/simba/troubleshooting/RI/project_folder/project_config.ini"
 VIDEO_NAME = "RI_01_165_clipped"
@@ -28,38 +30,68 @@ WHITE = "Animal_1"
 BLACK = "Animal_2"
 
 config = ConfigReader(config_path=CONFIG_PATH, create_logger=False)
-data_path = os.path.join(config.outlier_corrected_dir, f"{VIDEO_NAME}.{config.file_type}")
+data_path = os.path.join(
+    config.outlier_corrected_dir, f"{VIDEO_NAME}.{config.file_type}"
+)
 video_path = os.path.join(config.video_dir, f"{VIDEO_NAME}.mp4")
 get_video_meta_data(video_path)
 df = read_df(file_path=data_path, file_type=config.file_type)
 resolution, pixels_per_mm, fps = config.read_video_info(video_name=VIDEO_NAME)
 
-white_animal_bp_names, black_animal_bp_names = (config.animal_bp_dict[WHITE], config.animal_bp_dict[BLACK])
+white_animal_bp_names, black_animal_bp_names = (
+    config.animal_bp_dict[WHITE],
+    config.animal_bp_dict[BLACK],
+)
 white_animal_cols, black_animal_cols = [], []
-for x, y in zip(white_animal_bp_names["X_bps"], white_animal_bp_names["Y_bps"]): white_animal_cols.extend((x, y))
-for x, y in zip(black_animal_bp_names["X_bps"], black_animal_bp_names["Y_bps"]): black_animal_cols.extend((x, y))
-white_animal_df, black_animal_df = df[white_animal_cols].astype(int), df[black_animal_cols].astype(int)
+for x, y in zip(white_animal_bp_names["X_bps"], white_animal_bp_names["Y_bps"]):
+    white_animal_cols.extend((x, y))
+for x, y in zip(black_animal_bp_names["X_bps"], black_animal_bp_names["Y_bps"]):
+    black_animal_cols.extend((x, y))
+white_animal_df, black_animal_df = df[white_animal_cols].astype(int), df[
+    black_animal_cols
+].astype(int)
 white_animal_df_arr = white_animal_df.values.reshape(len(white_animal_df), -1, 2)
 black_animal_df_arr = black_animal_df.values.reshape(len(black_animal_df), -1, 2)
 
-white_animal_polygons = GeometryMixin().multiframe_bodyparts_to_polygon( data=white_animal_df_arr, pixels_per_mm=pixels_per_mm, parallel_offset=40, verbose=False, core_cnt=-1)
-black_animal_polygons = GeometryMixin().multiframe_bodyparts_to_polygon( data=black_animal_df_arr, pixels_per_mm=pixels_per_mm, parallel_offset=40, verbose=False, core_cnt=-1)
-white_animal_polygons = GeometryMixin().multiframe_minimum_rotated_rectangle(shapes=white_animal_polygons, verbose=False, core_cnt=-1)
-black_animal_polygons = GeometryMixin().multiframe_minimum_rotated_rectangle(shapes=black_animal_polygons, verbose=False, core_cnt=-1)
+white_animal_polygons = GeometryMixin().multiframe_bodyparts_to_polygon(
+    data=white_animal_df_arr,
+    pixels_per_mm=pixels_per_mm,
+    parallel_offset=40,
+    verbose=False,
+    core_cnt=-1,
+)
+black_animal_polygons = GeometryMixin().multiframe_bodyparts_to_polygon(
+    data=black_animal_df_arr,
+    pixels_per_mm=pixels_per_mm,
+    parallel_offset=40,
+    verbose=False,
+    core_cnt=-1,
+)
+white_animal_polygons = GeometryMixin().multiframe_minimum_rotated_rectangle(
+    shapes=white_animal_polygons, verbose=False, core_cnt=-1
+)
+black_animal_polygons = GeometryMixin().multiframe_minimum_rotated_rectangle(
+    shapes=black_animal_polygons, verbose=False, core_cnt=-1
+)
 
 # IF WE WANT, THEN WE CAN VISUALIZE THE ANIMAL GEOMETRIES.
-imgs = ImageMixin().slice_shapes_in_imgs(imgs=video_path, shapes=white_animal_polygons, verbose=False)
-ImageMixin.img_stack_to_video( imgs=imgs, save_path="/Users/simon/Desktop/envs/simba/troubleshooting/RI/project_folder/frames/output/stack/test.mp4", fps=fps)
+imgs = ImageMixin().slice_shapes_in_imgs(
+    imgs=video_path, shapes=white_animal_polygons, verbose=False
+)
+ImageMixin.img_stack_to_video(
+    imgs=imgs,
+    save_path="/Users/simon/Desktop/envs/simba/troubleshooting/RI/project_folder/frames/output/stack/test.mp4",
+    fps=fps,
+)
 
 im = imgs[4]
 height, width, depth = im.shape
 figsize = width / float(dpi), height / float(dpi)
 
 plt.figure(figsize=figsize)
-plt.axis('off')
+plt.axis("off")
 plt.imshow(im)
 plt.show()
-
 
 
 # Entire_video
