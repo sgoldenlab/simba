2b1a56c486bc751fec21da29ffd30c3e799b2644
sronilsson <sronilsson@gmail.com>
2024-05-13 14:55:59 -0400

    video tools

--- simba/video_processors/video_processing.py
+++ simba/video_processors/video_processing.py
@@ -3106,6 +3106,52 @@ def convert_to_mov(path: Union[str, os.PathLike],
     stdout_success(msg=f"{len(file_paths)} video(s) converted to MOV and saved in {save_dir} directory.", elapsed_time=timer.elapsed_time_str, source=convert_to_mov.__name__, )
 
 
+def overlay_video_progressbar(video_path: Union[str, os.PathLike],
+                              bar_height: Optional[int] = 10,
+                              color: Optional[str] = 'red',
+                              save_dir: Optional[ Union[str, os.PathLike]] = None) -> None:
+
+    """
+    Overlay a progress bar on a directory of videos or a single video.
+
+    .. video:: _static/img/overlay_video_progressbar.webm
+       :loop:
+
+    :param Union[str, os.PathLike] video_path: Directory containing video files or a single video file
+    :param Optional[int] bar_height: The height of the progressbar in percent of the video height.
+    :param Optional[str] color: The color of the progress bar. See simba.utils.lookups.get_color_dict keys for accepted names.
+    :param Optional[Union[str, os.PathLike]] save_dir: If not None, then saves the videos in the passed directory. Else, in the same directry with the ``_progress_bar`` suffix.
+    :return: None.
+    """
+
+    timer = SimbaTimer(start=True)
+    color = ''.join(filter(str.isalnum, color)).lower()
+    if os.path.isfile(video_path):
+        check_file_exist_and_readable(file_path=video_path)
+        video_paths = [video_path]
+    elif os.path.isdir(video_path):
+        video_path = find_all_videos_in_directory(directory=video_path, as_dict=True, raise_error=True)
+        video_paths = list(video_path.values())
+    else:
+        raise InvalidInputError(msg='{} is not a valid file path or directory path.', source=overlay_video_progressbar.__name__)
+    if save_dir is not None:
+        check_if_dir_exists(in_dir=save_dir)
+    else:
+        save_dir, _, _ = get_fn_ext(filepath=video_paths[0])
+    for cnt, video_path in enumerate(video_paths):
+        video_meta_data = get_video_meta_data(video_path=video_path)
+        video_length = video_meta_data['video_length_s']
+        width, height = video_meta_data['width'], video_meta_data['height']
+        bar_height = int(height * (bar_height/100))
+        _, video_name, ext = get_fn_ext(filepath=video_path)
+        print(f'Inserting progress bar on video {video_name}...')
+        save_path = os.path.join(save_dir, f'{video_name}_progress_bar{ext}')
+        check_int(name=f'{overlay_video_progressbar} height', value=bar_height, max_value=height, min_value=1)
+        cmd = f'ffmpeg -i "{video_path}" -filter_complex "color=c={color}:s={width}x{bar_height}[bar];[0][bar]overlay=-w+(w/{video_length})*t:H-h:shortest=1" -c:a copy "{save_path}" -loglevel error -stats -hide_banner -y'
+        subprocess.call(cmd, shell=True, stdout=subprocess.PIPE)
+    timer.stop_timer()
+    stdout_success(msg=f"{len(video_paths)} video(s) saved with progressbar in {save_dir} directory.", elapsed_time=timer.elapsed_time_str, source=overlay_video_progressbar.__name__, )
+
 
 # video_paths = ['/Users/simon/Desktop/envs/simba/troubleshooting/beepboop174/project_folder/merge/Trial    10_clipped_gantt.mp4',
 #                '/Users/simon/Desktop/envs/simba/troubleshooting/beepboop174/project_folder/merge/Trial    10_clipped.mp4',
