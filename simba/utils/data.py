cddf5f6103befa4875e1e1653757519ea803d323
sronilsson <sronilsson@users.noreply.github.com>
2024-06-22 20:01:02 +0000

    cleaned

--- simba/utils/data.py
+++ simba/utils/data.py
@@ -24,6 +24,7 @@ except:
     from typing_extensions import Literal
 
 from simba.utils.checks import (check_file_exist_and_readable, check_float,
+                                check_if_df_field_is_boolean,
                                 check_if_dir_exists,
                                 check_if_keys_exist_in_dict,
                                 check_if_module_has_import,
@@ -31,8 +32,7 @@ from simba.utils.checks import (check_file_exist_and_readable, check_float,
                                 check_if_valid_rgb_tuple, check_instance,
                                 check_int, check_str, check_that_column_exist,
                                 check_that_hhmmss_start_is_before_end,
-                                check_valid_array, check_valid_dataframe,
-                                check_if_df_field_is_boolean)
+                                check_valid_array, check_valid_dataframe)
 from simba.utils.enums import ConfigKey, Dtypes, Keys, Options
 from simba.utils.errors import (BodypartColumnNotFoundError, CountError,
                                 InvalidFileTypeError, InvalidInputError,
