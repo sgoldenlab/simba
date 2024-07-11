dabbc676c946b77c9748ec98dbea014183d3b8fb
sronilsson <sronilsson@users.noreply.github.com>
2024-07-11 15:23:59 +0000

    cleaned

--- simba/plotting/geometry_plotter.py
+++ simba/plotting/geometry_plotter.py
@@ -11,9 +11,10 @@ from shapely.geometry import (LineString, MultiLineString, MultiPolygon, Point,
 
 from simba.mixins.config_reader import ConfigReader
 from simba.mixins.plotting_mixin import PlottingMixin
-from simba.utils.checks import (check_file_exist_and_readable,
+from simba.utils.checks import (check_file_exist_and_readable, check_float,
                                 check_if_dir_exists, check_instance, check_int,
-                                check_iterable_length, check_str, check_valid_boolean, check_float)
+                                check_iterable_length, check_str,
+                                check_valid_boolean)
 from simba.utils.enums import Defaults, Formats, TextOptions
 from simba.utils.errors import InvalidInputError
 from simba.utils.lookups import get_color_dict
