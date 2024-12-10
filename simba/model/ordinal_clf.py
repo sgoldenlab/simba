2858e462d1df5c09f6776c978b75c417585e91a2
sronilsson <sronilsson@users.noreply.github.com>
2024-12-10 15:57:20 +0000

    cleaned

--- simba/model/ordinal_clf.py
+++ simba/model/ordinal_clf.py
@@ -1,14 +1,18 @@
 import os
-from typing import Union, Optional, Dict
-from sklearn.ensemble import RandomForestClassifier
+from typing import Dict, Optional, Union
+
 import numpy as np
 from joblib import Parallel, delayed
 from sklearn import clone
+from sklearn.ensemble import RandomForestClassifier
+
 from simba.mixins.train_model_mixin import TrainModelMixin
-from simba.utils.errors import SamplingError, InvalidInputError
-from simba.utils.checks import check_valid_array, check_int, check_if_dir_exists, check_file_exist_and_readable
+from simba.utils.checks import (check_file_exist_and_readable,
+                                check_if_dir_exists, check_int,
+                                check_valid_array)
 from simba.utils.enums import Formats
-from simba.utils.read_write import find_core_cnt, write_pickle, read_pickle
+from simba.utils.errors import InvalidInputError, SamplingError
+from simba.utils.read_write import find_core_cnt, read_pickle, write_pickle
 
 ACCEPTED_MODELS = RandomForestClassifier
 
