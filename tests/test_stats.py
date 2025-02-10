dca43d9b9eadfcbcdda0b2751ec4cb3610be274e
sronilsson <sronilsson@gmail.com>
2025-02-10 13:55:23 -0500

    roi opencv to tkinter

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -68,14 +68,14 @@ def test_rolling_one_way_anova():
     expected_results = np.array([[0.        ], [0.        ], [0.47496008], [0.47496008], [0.57924093], [0.57924093], [0.75560836], [0.75560836], [0.17295212], [0.17295212]])
     assert np.allclose(results, expected_results)
 
-@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 0.59), ("doane", 0.37), ("auto", 0.53), ("scott", 1.22), ("stone", 0.03), ("rice", 0.53), ("sturges", 0.53), ("sqrt", 0.66), ("auto",   0.53)])
+@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 0.59), ("doane", 0.37), ("auto", 0.53), ("scott", 1.22), ("rice", 0.53), ("sturges", 0.53), ("sqrt", 0.66), ("auto",   0.53)])
 def test_kullback_leibler_divergence(bucket_method, expected_results):
     sample_1 = np.array([1, 2, 3, 1, 3, 2, 1, 10, 8, 4, 10])
     sample_2 = np.array([8, 5, 5, 8, 8, 9, 10, 1, 7, 10, 10])
     results = Statistics().kullback_leibler_divergence(sample_1=sample_1, sample_2=sample_2, fill_value=1, bucket_method=bucket_method)
     assert np.round(results, 2) == expected_results
 
-@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 0.13), ("doane", 0.32), ("auto", 0.3), ("scott", 0.25), ("stone", 0.43), ("rice", 0.3), ("sturges", 0.3), ("sqrt", 0.28)])
+@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 0.13), ("doane", 0.32), ("auto", 0.3), ("scott", 0.25), ("rice", 0.3), ("sturges", 0.3), ("sqrt", 0.28)])
 def test_jensen_shannon_divergence(bucket_method, expected_results):
     sample_1 = np.array([1, 2, 3, 1, 3, 2, 1, 10, 8, 4, 10])
     sample_2 = np.array([8, 5, 5, 8, 8, 9, 10, 1, 7, 10, 10])
@@ -83,14 +83,14 @@ def test_jensen_shannon_divergence(bucket_method, expected_results):
     assert np.round(results, 2) == expected_results
 
 
-@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 0.05), ("doane", 0.03), ("auto", 0.03), ("scott", 0.12), ("stone", 0.0), ("rice", 0.03), ("sturges", 0.03), ("sqrt", 0.04)])
+@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 0.05), ("doane", 0.03), ("auto", 0.03), ("scott", 0.12), ("rice", 0.03), ("sturges", 0.03), ("sqrt", 0.04)])
 def test_wasserstein_distance(bucket_method, expected_results):
     sample_1 = np.array([1, 2, 3, 1, 3, 2, 1, 10, 8, 4, 10])
     sample_2 = np.array([8, 5, 5, 8, 8, 9, 10, 1, 7, 10, 10])
     results = Statistics().wasserstein_distance(sample_1=sample_1, sample_2=sample_2, bucket_method=bucket_method)
     assert np.round(results, 2) == expected_results
 
-@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 1.13), ("doane", 0.72), ("auto", 1.0), ("scott", 2.26), ("stone", 0.06), ("rice", 1.0), ("sturges", 1.0), ("sqrt", 1.24)])
+@pytest.mark.parametrize('bucket_method, expected_results', [("fd", 1.13), ("doane", 0.72), ("auto", 1.0), ("scott", 2.26), ("rice", 1.0), ("sturges", 1.0), ("sqrt", 1.24)])
 def test_population_stability_index(bucket_method, expected_results):
     sample_1 = np.array([1, 2, 3, 1, 3, 2, 1, 10, 8, 4, 10])
     sample_2 = np.array([8, 5, 5, 8, 8, 9, 10, 1, 7, 10, 10])
