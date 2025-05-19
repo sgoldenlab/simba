19dfbdb246ce88a98b02601ce21af1bdc7edec96
sronilsson <sronilsson@users.noreply.github.com>
2025-05-19 19:45:02 +0000

    cleaned

--- simba/plotting/data_plotter.py
+++ simba/plotting/data_plotter.py
@@ -1,20 +1,24 @@
 __author__ = "Simon Nilsson"
 
 import os
-from typing import Any, Dict, List, Optional, Union, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import cv2
 import numpy as np
 import pandas as pd
 from joblib import Parallel, delayed
 
-from simba.data_processors.timebins_movement_calculator import TimeBinsMovementCalculator
+from simba.data_processors.timebins_movement_calculator import \
+    TimeBinsMovementCalculator
 from simba.mixins.config_reader import ConfigReader
-from simba.utils.checks import (check_all_file_names_are_represented_in_video_log, check_valid_lst, check_file_exist_and_readable, check_str, check_if_valid_rgb_tuple, check_int, check_valid_tuple, check_valid_boolean)
+from simba.mixins.plotting_mixin import PlottingMixin
+from simba.utils.checks import (
+    check_all_file_names_are_represented_in_video_log,
+    check_file_exist_and_readable, check_if_valid_rgb_tuple, check_int,
+    check_str, check_valid_boolean, check_valid_lst, check_valid_tuple)
 from simba.utils.enums import Formats
 from simba.utils.errors import NoSpecifiedOutputError
 from simba.utils.printing import SimbaTimer, stdout_success
-from simba.mixins.plotting_mixin import PlottingMixin
 
 VELOCITY = 'Velocity (cm/s)'
 MEASUREMENT = 'MEASUREMENT'
