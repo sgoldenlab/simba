fdb64ef4c3ab22581d3a5b21dc7b3d5b67787e08
sronilsson <sronilsson@gmail.com>
2025-05-20 09:39:33 -0400

    tests

--- tests/test_data_processors.py
+++ tests/test_data_processors.py
@@ -10,20 +10,20 @@ from simba.data_processors.timebins_movement_calculator import TimeBinsMovementC
 
 
 
-@pytest.mark.parametrize("config_path, data_measures, video_meta_data, classifiers", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', ['Bout count'], [], ['Attack']),
-                                                                                      ('tests/data/test_projects/two_c57/project_folder/project_config.ini', ['Total event duration (s)'], [], ['Attack'])])
-def test_create_clf_log_use_case(config_path, data_measures, video_meta_data, classifiers):
-
-    clf_log_creator = AggregateClfCalculator(config_path=config_path,
-                                             data_measures=data_measures, 
-                                             classifiers=classifiers,
-                                             video_meta_data=video_meta_data)
-    clf_log_creator.run()
-    clf_log_creator.save()
-    assert len(clf_log_creator.results_df) == len(data_measures)
-    assert os.path.isfile(clf_log_creator.save_path)
-    os.remove(clf_log_creator.save_path)
-    
+# @pytest.mark.parametrize("config_path, data_measures, video_meta_data, classifiers", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', ['Bout count'], [], ['Attack']),
+#                                                                                       ('tests/data/test_projects/two_c57/project_folder/project_config.ini', ['Total event duration (s)'], [], ['Attack'])])
+# def test_create_clf_log_use_case(config_path, data_measures, video_meta_data, classifiers):
+#
+#     clf_log_creator = AggregateClfCalculator(config_path=config_path,
+#                                              data_measures=data_measures,
+#                                              classifiers=classifiers,
+#                                              video_meta_data=video_meta_data)
+#     clf_log_creator.run()
+#     clf_log_creator.save()
+#     assert len(clf_log_creator.results_df) == len(data_measures)
+#     assert os.path.isfile(clf_log_creator.save_path)
+#     os.remove(clf_log_creator.save_path)
+#
 
 @pytest.mark.parametrize("config_path, time_window, behavior_lst, create_graph", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', 2000, ['Attack', 'Sniffing'], False)])
 def test_fsttc_calculator_use_case(config_path, time_window, behavior_lst, create_graph):
@@ -57,15 +57,15 @@ def test_movement_calculator_use_case(config_path, body_parts, threshold):
     os.remove(calculator.save_path)
 
 
-@pytest.mark.parametrize("config_path, bin_length, measurements, classifiers", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', 2, ['Event count'], ['Attack'])])
-def test_time_bins_clf_calculator_use_case(config_path, bin_length, measurements, classifiers):
-    calculator = TimeBinsClfCalculator(config_path=config_path,
-                                       bin_length=bin_length,
-                                       measurements=measurements,
-                                       classifiers=classifiers)
-    calculator.run()
-    assert os.path.isfile(calculator.save_path)
-    os.remove(calculator.save_path)
+# @pytest.mark.parametrize("config_path, bin_length, measurements, classifiers", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', 2, ['Event count'], ['Attack'])])
+# def test_time_bins_clf_calculator_use_case(config_path, bin_length, measurements, classifiers):
+#     calculator = TimeBinsClfCalculator(config_path=config_path,
+#                                        bin_length=bin_length,
+#                                        measurements=measurements,
+#                                        classifiers=classifiers)
+#     calculator.run()
+#     assert os.path.isfile(calculator.save_path)
+#     os.remove(calculator.save_path)
 
 
 @pytest.mark.parametrize("config_path, bin_length, plots, body_parts", [('tests/data/test_projects/two_c57/project_folder/project_config.ini', 10, True, ['Nose_1', 'Nose_2'])])
