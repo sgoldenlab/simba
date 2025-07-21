dde158fdd97127dafce7674ce1559aec6e20b36c
sronilsson <sronilsson@gmail.com>
2025-07-21 15:58:21 -0400

    blob / facemap imports

--- simba/utils/lookups.py
+++ simba/utils/lookups.py
@@ -9,6 +9,7 @@ import subprocess
 import sys
 import tkinter as tk
 from copy import deepcopy
+from copy import copy
 from datetime import datetime
 from multiprocessing import Lock, Value
 from pathlib import Path
@@ -23,7 +24,7 @@ from matplotlib import cm
 
 import simba
 from simba.utils.checks import (check_file_exist_and_readable,
-                                check_if_dir_exists)
+                                check_if_dir_exists, check_valid_dict, check_str, check_int)
 from simba.utils.enums import OS, UML, FontPaths, Methods, Paths
 from simba.utils.read_write import get_fn_ext
 from simba.utils.warnings import NoDataFoundWarning
@@ -603,49 +604,69 @@ def get_labelling_img_kbd_bindings() -> dict:
 
 def get_labelling_video_kbd_bindings() -> dict:
     """
-    Returns dictionary of OpenCV keyboard bindings.
+    Returns a dictionary of OpenCV-compatible keyboard bindings for video labeling.
 
-    .. note::
-        Change ``kbd`` values to change keyboard shortcuts. Note that OpenCV keyboard bindings are different from tkinter
-        keyboard bindings as used in ``get_labelling_img_kbd_bindings``.
+    Notes:
+        - Change the `kbd` values to customize keyboard shortcuts.
+        - OpenCV key codes differ from Tkinter bindings (see `get_labelling_img_kbd_bindings`).
+        - Use either single-character strings (e.g. 'p') or integer ASCII codes (e.g. 32 for space bar).
 
-        Use either letters as below, or integer ASCII code for non-letters. For example:
-            - Space bar: 32
-            - Left arrow: 81
-            - Right arrow: 83
-
-        e.g., {'Pause/Play': {'label': 'p = Pause/Play', 'kbd': 32} would remap the space-bar to pause.
-
+    Examples:
+        Remap space bar to Pause/Play:
+            {'Pause/Play': {'label': 'Space = Pause/Play', 'kbd': 32}}
     """
 
+    bindings = {
+        'Pause/Play': {
+            'label': 'p = Pause/Play',
+            'kbd': 'p'
+        },
+        'forward_two_frames': {
+            'label': 'o = +2 frames',
+            'kbd': 'o'
+        },
+        'forward_ten_frames': {
+            'label': 'e = +10 frames',
+            'kbd': 'e'
+        },
+        'forward_one_second': {
+            'label': 'w = +1 second',
+            'kbd': 'w'
+        },
+        'backwards_two_frames': {
+            'label': 't = -2 frames',
+            'kbd': 't'
+        },
+        'backwards_ten_frames': {
+            'label': 's = -10 frames',
+            'kbd': 's'
+        },
+        'backwards_one_second': {
+            'label': 'x = -1 second',
+            'kbd': 'x'
+        },
+        'close_window': {
+            'label': 'q = Close video window',
+            'kbd': 'q'
+        },
+    }
 
-    return \
-        {'Pause/Play': #
-             {'label': 'p = Pause/Play',
-              'kbd': 'p'},
-         'forward_two_frames':  #
-             {'label': 'o = +2 frames',
-              'kbd': "o"},
-         'forward_ten_frames':  #
-             {'label': 'e = +10 frames',
-              'kbd': "e"},
-         'forward_one_second':  #
-             {'label': 'w = +1 second',
-              'kbd': "w"},
-         'backwards_two_frames':  #
-             {'label': 't = -2 frames',
-              'kbd': "t"},
-         'backwards_ten_frames':  #
-             {'label': 's = -10 frames',
-              'kbd': "s"},
-         'backwards_one_second':  #
-             {'label': 'x = -1 second',
-              'kbd': "x"},
-         'close_window':  #
-             {'label': 'q = Close video window',
-              'kbd': "q"},
-         }
 
+    #PERFORM CHECKS THAT BINDINGS ARE DEFINED CORRECTLY.
+    check_valid_dict( x=bindings, valid_key_dtypes=(str,), valid_values_dtypes=(dict,), source=f'{get_labelling_video_kbd_bindings.__name__} bindings')
+    cleaned_bindings = {}
+    for action, config in bindings.items():
+        check_valid_dict(x=config, valid_key_dtypes=(str,), valid_values_dtypes=(str, int), required_keys=('label', 'kbd'))
+        kbd_val = config['kbd']
+        check_str(value=config['label'], allow_blank=False, raise_error=True, name=f'{get_labelling_video_kbd_bindings.__name__} action')
+        if check_int(name=f'{action} kbd', value=kbd_val, raise_error=False)[0]:
+            new_config = copy(config)
+            new_config['kbd'] = int(kbd_val)
+            cleaned_bindings[action] = new_config
+        else:
+            cleaned_bindings[action] = config
+
+    return cleaned_bindings
 
 
 def get_fonts():
