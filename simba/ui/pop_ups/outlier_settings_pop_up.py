e2f9cafdfa28c17bcea0f7606f8ce38aa09a158a
sronilsson <sronilsson@users.noreply.github.com>
2025-11-17 19:38:22 +0000

    cleaned

--- simba/ui/pop_ups/outlier_settings_pop_up.py
+++ simba/ui/pop_ups/outlier_settings_pop_up.py
@@ -6,7 +6,8 @@ from typing import Union
 
 from simba.mixins.config_reader import ConfigReader
 from simba.mixins.pop_up_mixin import PopUpMixin
-from simba.ui.tkinter_functions import (CreateLabelFrameWithIcon, DropDownMenu, Entry_Box, SimbaButton, SimBADropDown)
+from simba.ui.tkinter_functions import (CreateLabelFrameWithIcon, DropDownMenu,
+                                        Entry_Box, SimbaButton, SimBADropDown)
 from simba.utils.checks import check_float
 from simba.utils.enums import ConfigKey, Formats, Keys, Links
 from simba.utils.printing import stdout_success
