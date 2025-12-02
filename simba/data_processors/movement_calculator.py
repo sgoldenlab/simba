4a9f81030ee16f8c252b425f8690a75a0ca9e6f2
sronilsson <sronilsson@gmail.com>
2025-12-01 21:22:41 -0500

    movement options

--- simba/data_processors/movement_calculator.py
+++ simba/data_processors/movement_calculator.py
@@ -10,25 +10,26 @@ import pandas as pd
 from simba.feature_extractors.perimeter_jit import jitted_centroid
 from simba.mixins.config_reader import ConfigReader
 from simba.mixins.feature_extraction_mixin import FeatureExtractionMixin
-from simba.mixins.feature_extraction_supplement_mixin import \
-    FeatureExtractionSupplemental
-from simba.utils.checks import (
-    check_all_file_names_are_represented_in_video_log, check_float, check_str,
-    check_that_column_exist, check_valid_lst)
-from simba.utils.errors import NoDataError
-from simba.utils.printing import stdout_success
-from simba.utils.read_write import (find_files_of_filetypes_in_directory,
-                                    get_fn_ext, read_df)
+from simba.mixins.feature_extraction_supplement_mixin import FeatureExtractionSupplemental
+from simba.utils.checks import (check_valid_boolean, check_all_file_names_are_represented_in_video_log, check_float, check_str, check_that_column_exist, check_valid_lst)
+from simba.utils.errors import NoDataError, InvalidInputError
+from simba.utils.printing import stdout_success, SimbaTimer
+from simba.utils.read_write import (find_files_of_filetypes_in_directory, get_fn_ext, read_df)
 
 
 class MovementCalculator(ConfigReader, FeatureExtractionMixin):
     """
     Compute aggregate movement statistics from pose-estimation data in SimBA project.
 
-    :parameters str config_path: path to SimBA project config file in Configparser format
+    :param Union[str, os.PathLike] config_path: path to SimBA project config file in Configparser format
     :param List[str] body_parts: Body-parts to use for movement calculations OR ``Animal_name CENTER OF GRAVITY``. If ``Animal_name CENTER OF GRAVITY``, then SimBA will approximate animal centroids through convex hull.
     :param float threshold: Filter body-part detection below set threshold (Value 0-1). Default: 0.00
-    :param List[str] or None file_paths: Files to calucalte movements for. If None, then all files in ``project_folder/csv/outlier_corrected_movement_location`` directory.
+    :param Optional[List[str]] file_paths: Files to calculate movements for. If None, then all files in ``project_folder/csv/outlier_corrected_movement_location`` directory.
+    :param Optional[Union[str, os.PathLike]] save_path: Path to save the movement log. If None, saves to ``project_folder/logs/Movement_log_{datetime}.csv``. Default: None
+    :param bool distance: If True, calculate distance metrics. Default: True
+    :param bool velocity: If True, calculate velocity metrics. Default: True
+    :param bool transpose: If True, transpose the results DataFrame. Default: False
+    :param bool verbose: If True, print progress messages. Default: True
 
     .. note::
        `Tutorial <https://github.com/sgoldenlab/simba/blob/master/docs/Scenario2.md#part-4--analyze-machine-results>`__.
@@ -48,7 +49,11 @@ class MovementCalculator(ConfigReader, FeatureExtractionMixin):
                  body_parts: List[str],
                  threshold: float = 0.00,
                  file_paths: Optional[List[str]] = None,
-                 save_path: Optional[Union[str, os.PathLike]] = None):
+                 save_path: Optional[Union[str, os.PathLike]] = None,
+                 distance: bool = True,
+                 velocity: bool = True,
+                 transpose: bool = False,
+                 verbose: bool = True):
 
         ConfigReader.__init__(self, config_path=config_path)
         FeatureExtractionMixin.__init__(self)
@@ -75,8 +80,15 @@ class MovementCalculator(ConfigReader, FeatureExtractionMixin):
             check_str(name=f'{self.__class__.__name__} save_path', value=save_path, raise_error=True)
             self.save_path = save_path
         check_float(name=f'{self.__class__.__name__} threshold', value=threshold, min_value=0.0, max_value=1.0)
+        check_valid_boolean(value=distance, source=f'{self.__class__.__name__} distance', raise_error=True)
+        check_valid_boolean(value=velocity, source=f'{self.__class__.__name__} velocity', raise_error=True)
+        check_valid_boolean(value=transpose, source=f'{self.__class__.__name__} transpose', raise_error=True)
+        check_valid_boolean(value=verbose, source=f'{self.__class__.__name__} verbose', raise_error=True)
+        if not distance and not velocity:
+            raise InvalidInputError(msg='distance AND velocity are both False. To compute movement metrics, set at least one value to True.', source=self.__class__.__name__)
+        self.distance, self.velocity = distance, velocity
         check_valid_lst(data=body_parts, source=f'{self.__class__.__name__} file_paths', min_len=1, valid_dtypes=(str,), valid_values=self.body_parts_lst)
-        self.body_parts, self.threshold, self.body_parts = file_paths, threshold, body_parts
+        self.body_parts, self.threshold, self.body_parts, self.transpose, self.verbose = file_paths, threshold, body_parts, transpose, verbose
 
     def __find_body_part_columns(self):
         self.body_parts_dict, self.bp_list = {}, []
@@ -92,9 +104,10 @@ class MovementCalculator(ConfigReader, FeatureExtractionMixin):
         check_all_file_names_are_represented_in_video_log(video_info_df=self.video_info_df, data_paths=self.file_paths)
         self.results = pd.DataFrame(columns=["VIDEO", "ANIMAL", "BODY-PART", "MEASURE", "VALUE"])
         for file_cnt, file_path in enumerate(self.file_paths):
+            video_timer = SimbaTimer(start=True)
             self.__find_body_part_columns()
             _, video_name, _ = get_fn_ext(file_path)
-            print(f"Analysing {video_name}... (Video {file_cnt+1}/{len(self.file_paths)})")
+            if self.verbose: print(f"Analysing {video_name}... (Video {file_cnt+1}/{len(self.file_paths)})")
             self.data_df = read_df(file_path=file_path, file_type=self.file_type)
             self.video_info, self.px_per_mm, self.fps = self.read_video_info(video_name=video_name)
             if self.bp_list:
@@ -106,8 +119,10 @@ class MovementCalculator(ConfigReader, FeatureExtractionMixin):
                         animal_df = animal_df[animal_df[animal_data["BODY-PART HEADERS"][-1]] >= self.threshold]
                     animal_df = animal_df.iloc[:, 0:2].reset_index(drop=True)
                     distance, velocity = FeatureExtractionSupplemental.distance_and_velocity(x=animal_df.values, fps=self.fps, pixels_per_mm=self.px_per_mm, centimeters=True)
-                    self.results.loc[len(self.results)] = [video_name, animal_data["ANIMAL NAME"], animal_data["BODY-PART"], "Distance (cm)", distance]
-                    self.results.loc[len(self.results)] = [ video_name, animal_data["ANIMAL NAME"], animal_data["BODY-PART"], "Velocity (cm/s)", velocity]
+                    if self.distance:
+                        self.results.loc[len(self.results)] = [video_name, animal_data["ANIMAL NAME"], animal_data["BODY-PART"], "Distance (cm)", distance]
+                    if self.velocity:
+                        self.results.loc[len(self.results)] = [video_name, animal_data["ANIMAL NAME"], animal_data["BODY-PART"], "Velocity (cm/s)", velocity]
             else:
                 for animal in self.body_parts:
                     animal_name = animal.split("CENTER OF GRAVITY")[0].strip()
@@ -116,14 +131,19 @@ class MovementCalculator(ConfigReader, FeatureExtractionMixin):
                     df = pd.DataFrame(jitted_centroid(points=np.reshape(z.values, (len(z / 2), -1, 2)).astype(np.float32)), columns=["X", "Y"])
                     df = self.dataframe_savgol_smoother(df=df, fps=self.fps).astype(int)
                     distance, velocity = FeatureExtractionSupplemental.distance_and_velocity(x=df.values, fps=self.fps, pixels_per_mm=self.px_per_mm, centimeters=True)
-                    self.results.loc[len(self.results)] = [video_name, animal_name, "GRAVITY CENTER", "Distance (cm)", distance]
-                    self.results.loc[len(self.results)] = [video_name, animal_name, "GRAVITY CENTER", "Velocity (cm/s)", velocity]
-
+                    if self.distance:
+                        self.results.loc[len(self.results)] = [video_name, animal_name, "GRAVITY CENTER", "Distance (cm)", distance]
+                    if self.velocity:
+                        self.results.loc[len(self.results)] = [video_name, animal_name, "GRAVITY CENTER", "Velocity (cm/s)", velocity]
+            video_timer.stop_timer()
+            if self.verbose: print(f'Movement analysis in video {video_name} complete (elapsed time: {video_timer.elapsed_time_str}s)')
 
     def save(self):
+        if self.transpose:
+            self.results = (self.results.assign(col=lambda x: x["ANIMAL"] + "_" + x["BODY-PART"] + "_" + x["MEASURE"]).pivot(index="VIDEO", columns="col", values="VALUE").reset_index())
         self.results.set_index("VIDEO").to_csv(self.save_path)
         self.timer.stop_timer()
-        stdout_success(msg=f"Movement log saved in {self.save_path}", elapsed_time=self.timer.elapsed_time_str)
+        if self.verbose: stdout_success(msg=f"Movement log saved in {self.save_path}", elapsed_time=self.timer.elapsed_time_str)
 
 
 if __name__ == "__main__" and not hasattr(sys, 'ps1'):
--- simba/ui/pop_ups/movement_analysis_pop_up.py
+++ simba/ui/pop_ups/movement_analysis_pop_up.py
@@ -1,36 +1,53 @@
+import os
+from typing import Union
+
 from copy import deepcopy
 from tkinter import *
 
 from simba.data_processors.movement_calculator import MovementCalculator
 from simba.mixins.config_reader import ConfigReader
 from simba.mixins.pop_up_mixin import PopUpMixin
-from simba.ui.tkinter_functions import (CreateLabelFrameWithIcon, DropDownMenu,
-                                        Entry_Box, SimbaButton, SimBADropDown)
+from simba.ui.tkinter_functions import (CreateLabelFrameWithIcon, SimbaCheckbox, Entry_Box, SimBADropDown)
 from simba.utils.checks import check_float
-from simba.utils.enums import ConfigKey, Formats, Keys, Links
-from simba.utils.errors import NoDataError
-
+from simba.utils.enums import ConfigKey, Keys, Links
+from simba.utils.errors import NoDataError, InvalidInputError
 
 class MovementAnalysisPopUp(ConfigReader, PopUpMixin):
-    def __init__(self, config_path: str):
+    def __init__(self,
+                 config_path: Union[str, os.PathLike]):
+
         ConfigReader.__init__(self, config_path=config_path, read_video_info=False)
         if len(self.outlier_corrected_paths) == 0:
             raise NoDataError(msg=f'No data files found in {self.outlier_corrected_dir} directory, cannot compute movement statistics.', source=self.__class__.__name__)
-        PopUpMixin.__init__(self, title="ANALYZE MOVEMENT", size=(400, 400), icon='run')
+        PopUpMixin.__init__(self, title="ANALYZE MOVEMENT", size=(500, 500), icon='run')
+
         self.animal_cnt_frm = CreateLabelFrameWithIcon(parent=self.main_frm, header="SELECT NUMBER OF ANIMALS", icon_name=Keys.DOCUMENTATION.value, icon_link=Links.DATA_ANALYSIS.value, padx=5, pady=5, relief='solid')
-        self.animal_cnt_dropdown = SimBADropDown(parent=self.animal_cnt_frm, label="# OF ANIMALS", label_width=30, dropdown_width=20, value=1, dropdown_options=list(range(1, self.animal_cnt + 1)), command=self.create_settings_frm)
-        self.animal_cnt_frm.grid(row=0, column=0, sticky=NW, padx=10, pady=10)
+        self.animal_cnt_dropdown = SimBADropDown(parent=self.animal_cnt_frm, label="# OF ANIMALS", label_width=20, dropdown_width=20, value=1, dropdown_options=list(range(1, self.animal_cnt + 1)), command=self.create_settings_frm)
+        self.animal_cnt_frm.grid(row=0, column=0, sticky=NW)
         self.animal_cnt_dropdown.grid(row=0, column=0, sticky=NW)
 
         self.choose_threshold_frm = CreateLabelFrameWithIcon(parent=self.main_frm, header="SELECT BODY-PART THRESHOLD", icon_name='threshold', icon_link=Links.DATA_ANALYSIS.value, padx=5, pady=5, relief='solid')
-        self.probability_entry = Entry_Box(parent=self.choose_threshold_frm, fileDescription='THRESHOLD: ', labelwidth=30, entry_box_width=20, value=0.0, justify='center')
-        self.choose_threshold_frm.grid(row=1, column=0, sticky=NW, padx=10, pady=10)
+        self.probability_entry = Entry_Box(parent=self.choose_threshold_frm, fileDescription='THRESHOLD: ', labelwidth=20, entry_box_width=20, value=0.0, justify='center')
+        self.choose_threshold_frm.grid(row=1, column=0, sticky=NW)
         self.probability_entry.grid(row=0, column=0, sticky=NW)
 
         self.body_part_options = deepcopy(self.body_parts_lst)
         for i in self.multi_animal_id_list: self.body_part_options.append(f"{i} CENTER OF GRAVITY")
         self.create_settings_frm(animal_cnt=1)
-        self.create_run_frm(run_function=self.run, padx=10, pady=10)
+
+        self.measurments_frm = CreateLabelFrameWithIcon(parent=self.main_frm, header="MEASUREMENTS", icon_name='ruler', icon_link=Links.DATA_ANALYSIS.value, padx=5, pady=5, relief='solid')
+        distance_cb, self.distance_var = SimbaCheckbox(parent=self.measurments_frm, txt='DISTANCE (CM)', txt_img='distance', val=True)
+        velocity_cb, self.velocity_var = SimbaCheckbox(parent=self.measurments_frm, txt='VELOCITY (CM/S)', txt_img='run', val=True)
+        self.measurments_frm.grid(row=3, column=0, sticky=NW)
+        distance_cb.grid(row=0, column=0, sticky=NW)
+        velocity_cb.grid(row=1, column=0, sticky=NW)
+
+        self.transpose_frm = CreateLabelFrameWithIcon(parent=self.main_frm, header="TRANSPOSE OUTPUT CSV", icon_name='rotate', icon_link=Links.DATA_ANALYSIS.value, padx=5, pady=5, relief='solid')
+        transpose_cb, self.transpose_var = SimbaCheckbox(parent=self.transpose_frm, txt='TRANSPOSE OUTPUT CSV', txt_img='rotate', val=False)
+        self.transpose_frm.grid(row=4, column=0, sticky=NW)
+        transpose_cb.grid(row=0, column=0, sticky=NW)
+
+        self.create_run_frm(run_function=self.run)
         self.main_frm.mainloop()
 
     def create_settings_frm(self, animal_cnt):
@@ -42,9 +59,9 @@ class MovementAnalysisPopUp(ConfigReader, PopUpMixin):
         self.bp_frm = CreateLabelFrameWithIcon(parent=self.main_frm, header="SELECT BODY-PARTS", icon_name='pose', icon_link=Links.DATA_ANALYSIS.value, padx=5, pady=5, relief='solid')
         self.body_parts_dropdowns = {}
         for cnt, i in enumerate(range(int(animal_cnt))):
-            self.body_parts_dropdowns[cnt] = SimBADropDown(parent=self.bp_frm, label=f"Animal {cnt+1}", label_width=30, dropdown_width=20, value=self.body_part_options[cnt], dropdown_options=self.body_part_options)
+            self.body_parts_dropdowns[cnt] = SimBADropDown(parent=self.bp_frm, label=f"Animal {cnt+1}", label_width=20, dropdown_width=20, value=self.body_part_options[cnt], dropdown_options=self.body_part_options)
             self.body_parts_dropdowns[cnt].grid(row=cnt, column=0, sticky=NW)
-        self.bp_frm.grid(row=2, column=0, sticky=NW, padx=10, pady=10)
+        self.bp_frm.grid(row=2, column=0, sticky=NW)
 
     def run(self):
         self.config.set(ConfigKey.PROCESS_MOVEMENT_SETTINGS.value, ConfigKey.ROI_ANIMAL_CNT.value, str(self.animal_cnt_dropdown.getChoices()))
@@ -54,12 +71,18 @@ class MovementAnalysisPopUp(ConfigReader, PopUpMixin):
         threshold = float(self.probability_entry.entry_get)
         for cnt, dropdown in self.body_parts_dropdowns.items():
             body_parts.append(dropdown.getChoices())
+        distance, velocity, transpose = self.distance_var.get(), self.velocity_var.get(), self.transpose_var.get()
+        if not distance and not velocity:
+            raise InvalidInputError(msg='distance AND velocity are both un-checked. To compute movement metrics, check at least one value.', source=self.__class__.__name__)
         movement_processor = MovementCalculator(config_path=self.config_path,
                                                 threshold=threshold,
-                                                body_parts=body_parts)
+                                                body_parts=body_parts,
+                                                distance=distance,
+                                                velocity=velocity,
+                                                transpose=transpose)
         movement_processor.run()
         movement_processor.save()
 
 
 # MovementAnalysisPopUp(config_path=r"C:\troubleshooting\mitra\project_folder\project_config.ini")
-#MovementAnalysisPopUp(config_path='/Users/simon/Desktop/envs/simba/troubleshooting/two_black_animals_14bp/project_folder/project_config.ini')
+#_ = MovementAnalysisPopUp(config_path='/Users/simon/Desktop/envs/simba/troubleshooting/two_black_animals_14bp/project_folder/project_config.ini')
