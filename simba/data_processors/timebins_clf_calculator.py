2bbd61486f5bdf2bf913baacb3ddbe3d3d378813
sronilsson <sronilsson@users.noreply.github.com>
2026-02-07 20:40:33 +0000

    cleaned

--- simba/data_processors/timebins_clf_calculator.py
+++ simba/data_processors/timebins_clf_calculator.py
@@ -20,7 +20,7 @@ from simba.utils.checks import (
     check_valid_boolean, check_valid_lst)
 from simba.utils.data import detect_bouts
 from simba.utils.errors import NoDataError
-from simba.utils.printing import stdout_success, stdout_information
+from simba.utils.printing import stdout_information, stdout_success
 from simba.utils.read_write import (find_files_of_filetypes_in_directory,
                                     find_time_stamp_from_frame_numbers,
                                     get_fn_ext, read_df)
--- simba/plotting/gantt_creator_mp.py
+++ simba/plotting/gantt_creator_mp.py
@@ -43,14 +43,14 @@ from simba.mixins.config_reader import ConfigReader
 from simba.mixins.plotting_mixin import PlottingMixin
 from simba.utils.checks import (
     check_all_file_names_are_represented_in_video_log,
-    check_file_exist_and_readable, check_int, check_str,
-    check_that_column_exist, check_valid_boolean, check_valid_lst, check_float)
+    check_file_exist_and_readable, check_float, check_int, check_str,
+    check_that_column_exist, check_valid_boolean, check_valid_lst)
 from simba.utils.data import (create_color_palette, detect_bouts, get_cpu_pool,
                               terminate_cpu_pool)
 from simba.utils.enums import Formats, Options
 from simba.utils.errors import NoSpecifiedOutputError
 from simba.utils.lookups import get_fonts
-from simba.utils.printing import SimbaTimer, stdout_success, stdout_information
+from simba.utils.printing import SimbaTimer, stdout_information, stdout_success
 from simba.utils.read_write import (concatenate_videos_in_folder,
                                     create_directory, find_core_cnt,
                                     get_current_time, get_fn_ext, read_df)
