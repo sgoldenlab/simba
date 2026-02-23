088359d942c16daf21a542cbe72ae6704036fd30
sronilsson <sronilsson@users.noreply.github.com>
2026-02-23 20:21:13 +0000

    cleaned

--- simba/ui/pop_ups/yolo_plot_results.py
+++ simba/ui/pop_ups/yolo_plot_results.py
@@ -11,12 +11,14 @@ from simba.ui.tkinter_functions import (CreateLabelFrameWithIcon, FileSelect,
                                         SimBADropDown)
 from simba.utils.checks import (check_file_exist_and_readable,
                                 check_if_dir_exists)
+from simba.utils.data import get_cpu_pool, terminate_cpu_pool
 from simba.utils.enums import Options
 from simba.utils.errors import NoDataError
 from simba.utils.printing import stdout_warning
-from simba.utils.read_write import (find_core_cnt, find_files_of_filetypes_in_directory, get_video_meta_data, str_2_bool)
+from simba.utils.read_write import (find_core_cnt,
+                                    find_files_of_filetypes_in_directory,
+                                    get_video_meta_data, str_2_bool)
 from simba.utils.warnings import MissingFileWarning
-from simba.utils.data import get_cpu_pool, terminate_cpu_pool
 
 MAX_TRACKS_OPTIONS = ['None', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 CORE_CNT_OPTIONS = list(range(1, find_core_cnt()[0]))
