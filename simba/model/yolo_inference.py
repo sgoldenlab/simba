2b7ccba4b66a79b98655978d01784275c5c47558
sronilsson <sronilsson@users.noreply.github.com>
2026-08-03 13:00:30 +0000

    cleaned

--- simba/model/yolo_inference.py
+++ simba/model/yolo_inference.py
@@ -24,8 +24,8 @@ from simba.third_party_label_appenders.converters import \
 from simba.utils.checks import (check_file_exist_and_readable, check_float,
                                 check_if_dir_exists, check_instance, check_int,
                                 check_str, check_valid_boolean,
-                                check_valid_lst, check_valid_tuple,
-                                get_fn_ext, is_torch_cuda_available)
+                                check_valid_lst, check_valid_tuple, get_fn_ext,
+                                is_torch_cuda_available)
 from simba.utils.data import df_smoother, savgol_smoother
 from simba.utils.enums import Formats, Options
 from simba.utils.errors import (InvalidVideoFileError, SimBAGPUError,
