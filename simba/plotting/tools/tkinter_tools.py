1f143d183112e3ebb833f4d0d0e70828552a22a8
sronilsson <sronilsson@users.noreply.github.com>
2026-08-26 17:31:03 +0000

    cleaned

--- simba/plotting/tools/tkinter_tools.py
+++ simba/plotting/tools/tkinter_tools.py
@@ -1,27 +1,32 @@
 __author__ = "Simon Nilsson; sronilsson@gmail.com"
 
 import os
+from copy import deepcopy
 from tkinter import *
-from typing import Union, Optional
+from typing import Optional, Union
 
 import cv2
 import numpy as np
 import pandas as pd
 from PIL import Image as Img
 from PIL import ImageTk
-from copy import deepcopy
 
 from simba.mixins.config_reader import ConfigReader
 from simba.mixins.plotting_mixin import PlottingMixin
-from simba.utils.checks import check_file_exist_and_readable, check_valid_array, check_valid_boolean, check_valid_dataframe
-from simba.utils.enums import Formats
-from simba.utils.errors import InvalidInputError
-from simba.utils.lookups import get_icons_paths, get_simba_font_name_and_path, get_color_dict
-from simba.utils.enums import TextOptions
+from simba.ui.tkinter_functions import (CreateLabelFrameWithIcon, Entry_Box,
+                                        SimbaButton, SimBADropDown, SimBALabel,
+                                        SimBAScaleBar)
+from simba.utils.checks import (check_file_exist_and_readable,
+                                check_valid_array, check_valid_boolean,
+                                check_valid_dataframe)
 from simba.utils.data import create_color_palettes
-from simba.utils.read_write import get_video_meta_data, read_frm_of_video, str_2_bool
+from simba.utils.enums import Formats, TextOptions
+from simba.utils.errors import InvalidInputError
+from simba.utils.lookups import (get_color_dict, get_icons_paths,
+                                 get_simba_font_name_and_path)
+from simba.utils.read_write import (get_video_meta_data, read_frm_of_video,
+                                    str_2_bool)
 from simba.utils.warnings import FrameRangeWarning
-from simba.ui.tkinter_functions import SimBALabel, SimBAScaleBar, SimbaButton, Entry_Box, CreateLabelFrameWithIcon, SimBADropDown
 
 PADDING = 5
 MAX_SIZE = (1080, 650)
