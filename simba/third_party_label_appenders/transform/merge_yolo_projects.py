a87e6f6fbc2de8a2f1734dd1e2221e975adfda45
sronilsson <sronilsson@gmail.com>
2026-09-07 16:12:56 -0400

    mobile rdt x 8

--- simba/third_party_label_appenders/transform/merge_yolo_projects.py
+++ simba/third_party_label_appenders/transform/merge_yolo_projects.py
@@ -207,8 +207,8 @@ class MergeYoloProjects:
             stdout_information(msg=f'{display_name}: copied {total}/{total} complete.')
         return total
 
-merger = MergeYoloProjects(yaml_paths=[r"G:\netholabs\batch_0731\food_pellet\map.yaml",
-                                       r"G:\netholabs\batch_0731\food_pellet_2\map.yaml"],
-                           save_dir=r"G:\netholabs\batch_0731\food_pellet_3",
-                           train_val_split=0.75)
-merger.run()
+# merger = MergeYoloProjects(yaml_paths=[r"G:\netholabs\batch_0731\food_pellet\map.yaml",
+#                                        r"G:\netholabs\batch_0731\food_pellet_2\map.yaml"],
+#                            save_dir=r"G:\netholabs\batch_0731\food_pellet_3",
+#                            train_val_split=0.75)
+# merger.run()
